7e6ec56cf39768bd9ae604a19293e52975cbc03e
andi611 <tingweiandyliu@gmail.com>
2019-07-25 15:20:56 +0800

    acoustic preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,9 @@
 """*********************************************************************************************"""
 #   FileName     [ preprocess.py ]
 #   Synopsis     [ preprocess text transcripts and audio speech for the LibriSpeech dataset ]
-#   Author       [ Ting-Wei Liu (Andi611) ]
+#   Author       [ Andy T. Liu (Andi611) ]
 #   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+#   Reference    [ https://github.com/Alexander-H-Liu/End-to-end-ASR-Pytorch ]
 """*********************************************************************************************"""
 
 
@@ -19,14 +20,16 @@ import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
 from joblib import Parallel, delayed
-from src.preprocess import extract_feature,encode_target
-sys.path.insert(0, '..')
+from utils.asr import encode_target
+from utils.audio import extract_feature
+# sys.path.insert(0, '..')
 
 
 ##################
 # BOOLEAB STRING #
 ##################
 def boolean_string(s):
+	
 	if s not in ['False', 'True']:
 		raise ValueError('Not a valid boolean string')
 	return s == 'True'
@@ -36,9 +39,10 @@ def boolean_string(s):
 # PREPROCESS CONFIGURATIONS #
 #############################
 def get_preprocess_args():
+	
 	parser = argparse.ArgumentParser(description='preprocess arguments for LibriSpeech dataset.')
 
-	parser.add_argument('--data_path', type=str, help='Path to raw LibriSpeech dataset')
+	parser.add_argument('--data_path', default='./data/', type=str, help='Path to raw LibriSpeech dataset')
 	parser.add_argument('--output_path', default='.', type=str, help='Path to store output', required=False)
 
 	parser.add_argument('--feature_type', default='fbank', type=str, help='Feature type ( mfcc / fbank )', required=False)
@@ -56,7 +60,11 @@ def get_preprocess_args():
 	return args
 
 
+#############
+# READ TEXT #
+#############
 def read_text(file,target):
+	
 	src_file = '-'.join(file.split('-')[:-1])+'.trans.txt'
 	idx = file.split('/')[-1].split('.')[0]
 	if target =='char':
@@ -70,18 +78,14 @@ def read_text(file,target):
 				if idx == line.split(' ')[0]:
 					return line[:-1].split(' ',1)[1]
 	else:
-		raise ValueError('Unsupported target: '+target)
+		raise ValueError('Unsupported target: ' + target)
 
 
-
-# Process data
-sets = ['train-clean-100','train-clean-360','train-other-500','dev-clean','dev-other','test-clean','test-other']
-encode_table = None
-output_dir = None
-dim = args.feature_dim*(1+args.apply_delta+args.apply_delta_delta)
-
-# BPE training
-if args.target == 'subword':
+######################
+# SUBWORD PREPROCESS #
+######################
+def subword_preprocess(args, sets, encode_table, output_dir, dim):
+	
 	# Setup path
 	output_dir = os.path.join(args.output_path,'_'.join(['libri',str(args.feature_type)+str(dim),str(args.target)+str(args.n_tokens)]))
 	if not os.path.exists(output_dir):os.makedirs(output_dir)
@@ -113,6 +117,7 @@ if args.target == 'subword':
 		  '--vocab_size='+str(args.n_tokens),
 		  '--character_coverage=1.0'
 		])
+
 	# Encode data
 	if not os.path.exists(os.path.join(bpe_dir,'raw')):os.makedirs(os.path.join(bpe_dir,'raw'))
 	if not os.path.exists(os.path.join(bpe_dir,'encode')):os.makedirs(os.path.join(bpe_dir,'encode'))
@@ -134,52 +139,50 @@ if args.target == 'subword':
 			tok = line.split('\t')[0]
 			if tok not in ['<s>','</s>']:
 				encode_table[tok] = len(encode_table)
-
-print('')
-print('Data sets :')
-for idx,s in enumerate(sets):
-	print('\t',idx,':',s)
-tr_set = input('Please enter the index of splits you wish to use preprocess. (seperate with space): ')
-tr_set = [sets[int(t)] for t in tr_set.split(' ')]
+	return encode_table, output_dir, bpe_dir
 
 
-# Acoustic Feature Extraction & Make Date Table
-for s in tr_set:
-	print('')
-	print('Preprocessing',s,'data...',end='')
-	todo = list(Path(os.path.join(args.data_path,s)).rglob("*.flac"))
-	print(len(todo),'audio files found in',s)
+#######################
+# ACOUSTIC PREPROCESS #
+#######################
+def acoustic_preprocess(args, tr_set, encode_table, output_dir, bpe_dir, dim):
+	
+	for s in tr_set:
+		print('')
+		print('Preprocessing',s,'data...',end='')
+		todo = list(Path(os.path.join(args.data_path,s)).rglob("*.flac"))
+		print(len(todo),'audio files found in',s)
 
-	print('Encoding target...',flush=True)
-	if args.target == 'subword':
-		tr_y = []
-		with open(os.path.join(bpe_dir,'encode',s+'.txt'),'r') as f:
-			for line in f:tr_y.append(line[:-1].split(' '))
-	else:
-		tr_y = Parallel(n_jobs=args.n_jobs)(delayed(read_text)(str(file),target=args.target) for file in tqdm(todo))
-	tr_y, encode_table = encode_target(tr_y,table=encode_table,mode=args.target,max_idx=args.n_tokens)
+		print('Encoding target...',flush=True)
+		if args.target == 'subword':
+			tr_y = []
+			with open(os.path.join(bpe_dir,'encode',s+'.txt'),'r') as f:
+				for line in f:tr_y.append(line[:-1].split(' '))
+		else:
+			tr_y = Parallel(n_jobs=args.n_jobs)(delayed(read_text)(str(file),target=args.target) for file in tqdm(todo))
+		tr_y, encode_table = encode_target(tr_y,table=encode_table,mode=args.target,max_idx=args.n_tokens)
 
-	if output_dir is None:
-		output_dir = os.path.join(args.output_path,'_'.join(['libri',str(args.feature_type)+str(dim),str(args.target)+str(len(encode_table))]))
-	if not os.path.exists(output_dir):os.makedirs(output_dir)
-	cur_path = os.path.join(output_dir,s)
-	if not os.path.exists(cur_path):os.makedirs(cur_path)
+		if output_dir is None:
+			output_dir = os.path.join(args.output_path,'_'.join(['libri',str(args.feature_type)+str(dim),str(args.target)+str(len(encode_table))]))
+		if not os.path.exists(output_dir):os.makedirs(output_dir)
+		cur_path = os.path.join(output_dir,s)
+		if not os.path.exists(cur_path):os.makedirs(cur_path)
 
-	print('Extracting acoustic feature...',flush=True)
-	tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_feature)(str(file),feature=args.feature_type,dim=args.feature_dim,\
-			cmvn=args.apply_cmvn,delta=args.apply_delta,delta_delta=args.apply_delta_delta,save_feature=os.path.join(cur_path,str(file).split('/')[-1].replace('.flac',''))) for file in tqdm(todo))
+		print('Extracting acoustic feature...',flush=True)
+		tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_feature)(str(file),feature=args.feature_type,dim=args.feature_dim,\
+				cmvn=args.apply_cmvn,delta=args.apply_delta,delta_delta=args.apply_delta_delta,save_feature=os.path.join(cur_path,str(file).split('/')[-1].replace('.flac',''))) for file in tqdm(todo))
 
-	# sort by len
-	sorted_y = ['_'.join([str(i) for i in tr_y[idx]]) for idx in reversed(np.argsort(tr_x))]
-	sorted_todo = [os.path.join(s,str(todo[idx]).split('/')[-1].replace('.flac','.npy')) for idx in reversed(np.argsort(tr_x))]
-	# Dump label
-	df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo],'length':list(reversed(sorted(tr_x))),'label':sorted_y})
-	df.to_csv(os.path.join(output_dir,s+'.csv'))
+		# sort by len
+		sorted_y = ['_'.join([str(i) for i in tr_y[idx]]) for idx in reversed(np.argsort(tr_x))]
+		sorted_todo = [os.path.join(s,str(todo[idx]).split('/')[-1].replace('.flac','.npy')) for idx in reversed(np.argsort(tr_x))]
+		# Dump label
+		df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo],'length':list(reversed(sorted(tr_x))),'label':sorted_y})
+		df.to_csv(os.path.join(output_dir,s+'.csv'))
 
-	with open(os.path.join(output_dir,"mapping.pkl"), "wb") as fp:
-		pickle.dump(encode_table, fp)
+		with open(os.path.join(output_dir,"mapping.pkl"), "wb") as fp:
+			pickle.dump(encode_table, fp)
 
-print('All done, saved at',output_dir,'exit.')
+	print('All done, saved at', output_dir, 'exit.')
 
 
 ########
@@ -187,21 +190,29 @@ print('All done, saved at',output_dir,'exit.')
 ########
 def main():
 
+	# get arguments
 	args = get_preprocess_args()
 
-	if args.mode == 'all' or args.mode == 'make' or args.mode == 'analyze':
-		
-		#---preprocess text and data to be model ready---#
-		if args.mode == 'all' or args.mode == 'make':
-			make_meta(args.text_input_path, args.audio_input_dir, args.meta_dir, args.meta_text, args.file_suffix, args.num_workers, config.frame_shift_ms)
+	# Process data
+	sets = ['train-clean-100','train-clean-360','train-other-500','dev-clean','dev-other','test-clean','test-other']
+	encode_table = None
+	output_dir = None
+	dim = args.feature_dim*(1+args.apply_delta+args.apply_delta_delta)
 
-		#---dataset analyze---#
-		if args.mode == 'all' or args.mode == 'analyze':
-			dataset_analysis(args.audio_input_dir, args.file_suffix)
-	
-	else:
-		raise RuntimeError('Invalid mode!')
+	# BPE training
+	if args.target == 'subword':
+		encode_table, output_dir = subword_preprocess(args, sets, encode_table, output_dir, dim)
 
+	# Select data sets
+	print('')
+	print('Data sets :')
+	for idx,s in enumerate(sets):
+		print('\t',idx,':',s)
+	tr_set = input('Please enter the index of splits you wish to use preprocess. (seperate with space): ')
+	tr_set = [sets[int(t)] for t in tr_set.split(' ')]
+
+	# Acoustic Feature Extraction & Make Date Table
+	acoustic_preprocess(args, tr_set, encode_table, output_dir, bpe_dir, dim)
 
 
 if __name__ == '__main__':
---
+++ utils/audio.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ utils/audio.py ]
+#   Synopsis     [ audio processing functions ]
+#   Author       [ Andy T. Liu (Andi611) ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+#   Reference 1  [ https://github.com/Alexander-H-Liu/End-to-end-ASR-Pytorch ]
+#   Reference 2  [ https://groups.google.com/forum/#!msg/librosa/V4Z1HpTKn8Q/1-sMpjxjCSoJ ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+import librosa
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+# NOTE: there are warnings for MFCC extraction due to librosa's issue
+
+
+###################
+# EXTRACT FEATURE #
+###################
+# Acoustic Feature Extraction
+# Parameters
+#     - input file  : str, audio file path
+#     - feature     : str, fbank or mfcc
+#     - dim         : int, dimension of feature
+#     - cmvn        : bool, apply CMVN on feature
+#     - window_size : int, window size for FFT (ms)
+#     - stride      : int, window stride for FFT
+#     - save_feature: str, if given, store feature to the path and return len(feature)
+# Return
+#     acoustic features with shape (time step, dim)
+def extract_feature(input_file,feature='fbank',dim=40, cmvn=True, delta=False, delta_delta=False,
+					window_size=25, stride=10,save_feature=None):
+	y, sr = librosa.load(input_file,sr=None)
+	ws = int(sr*0.001*window_size)
+	st = int(sr*0.001*stride)
+	if feature == 'fbank': # log-scaled
+		feat = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=dim,
+									n_fft=ws, hop_length=st)
+		feat = np.log(feat+1e-6)
+	elif feature == 'mfcc':
+		feat = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=dim, n_mels=26,
+									n_fft=ws, hop_length=st)
+		feat[0] = librosa.feature.rmse(y, hop_length=st, frame_length=ws) 
+		
+	else:
+		raise ValueError('Unsupported Acoustic Feature: '+feature)
+
+	feat = [feat]
+	if delta:
+		feat.append(librosa.feature.delta(feat[0]))
+
+	if delta_delta:
+		feat.append(librosa.feature.delta(feat[0],order=2))
+	feat = np.concatenate(feat,axis=0)
+	if cmvn:
+		feat = (feat - feat.mean(axis=1)[:,np.newaxis]) / (feat.std(axis=1)+1e-16)[:,np.newaxis]
+	if save_feature is not None:
+		tmp = np.swapaxes(feat,0,1).astype('float32')
+		np.save(save_feature,tmp)
+		return len(tmp)
+	else:
+		return np.swapaxes(feat,0,1).astype('float32')
+
+		
