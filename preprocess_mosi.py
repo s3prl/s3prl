f03eb4675dd48a489a47a419ee18b098c899d031
Leo Yang <leo19941227@gmail.com>
2019-09-18 05:14:11 +0800

    MOSI data preprocess done

--- preprocess_mosi.py
+++ preprocess_mosi.py
@@ -23,6 +23,12 @@ from joblib import Parallel, delayed
 from utils.asr import encode_target
 from utils.audio import extract_feature, mel_dim, num_freq
 
+# using CMU SDK to access audio label
+# for installing mmsdk package, please follow the instructions in:
+# https://github.com/A2Zadeh/CMU-MultimodalSDK#installation
+import mmsdk
+from mmsdk import mmdatasdk as md
+
 
 ##################
 # BOOLEAB STRING #
@@ -32,66 +38,100 @@ def boolean_string(s):
 		raise ValueError('Not a valid boolean string')
 	return s == 'True'
 
+def sdk2npy(string):
+	# not data type conversion
+	# convert the name format in CMU sdk to corresponding npy file name
+	split = string.split('[')
+	utterance_name = split[0]
+	number = int(split[1].split(']')[0])
+	string = utterance_name + '_' + str(number + 1) + '.npy'
+	return string
+
+def npy2sdk(string):
+	# not data type conversion
+	# inverse operation of sdk2npy
+	split = string.split('_')
+	number = int(split[-1][:-4])
+	utterance_name = '_'.join(split[:-1])
+	string = utterance_name + '[' + str(number - 1) + ']'
+	return string
 
 #############################
 # PREPROCESS CONFIGURATIONS #
 #############################
 def get_preprocess_args():
-	
 	parser = argparse.ArgumentParser(description='preprocess arguments for LibriSpeech dataset.')
-
-	parser.add_argument('--data_path', default='./data/', type=str, help='Path to raw LibriSpeech dataset')
+	parser.add_argument('--data_path', default='/home/leo/d/workspace/dataset/MOSI/Raw/Audio/WAV_16000/Segmented', type=str, help='Path to raw MOSI segmented audio dataset')
 	parser.add_argument('--output_path', default='./data/', type=str, help='Path to store output', required=False)
-
 	parser.add_argument('--feature_type', default='fbank', type=str, help='Feature type ( mfcc / fbank / mel / linear )', required=False)
 	parser.add_argument('--apply_cmvn', default=True, type=boolean_string, help='Apply CMVN on feature', required=False)
-
 	parser.add_argument('--n_jobs', default=-1, type=int, help='Number of jobs used for feature extraction', required=False)
 	parser.add_argument('--n_tokens', default=5000, type=int, help='Vocabulary size of target', required=False)
-	parser.add_argument('--target', default='subword', type=str, help='Learning target ( phoneme / char / subword / word )', required=False)
-
 	args = parser.parse_args()
 	return args
 
 #######################
 # ACOUSTIC PREPROCESS #
 #######################
-def acoustic_preprocess(args, tr_set, encode_table, output_dir, bpe_dir, dim):
-	
-	for s in tr_set:
-		print('')
-		print('Preprocessing',s,'data...',end='')
-		todo = list(Path(os.path.join(args.data_path,s)).rglob("*.flac"))
-		print(len(todo),'audio files found in',s)
+def acoustic_preprocess(args, dim):
+	# Extracting features from audio
+	todo = list(Path(args.data_path).glob("*.wav"))
+	print(len(todo),'audio files found in MOSI')
 
-		print('Encoding target...',flush=True)
-		if args.target == 'subword':
-			tr_y = []
-			with open(os.path.join(bpe_dir,'encode', s+'.txt'), 'r') as f:
-				for line in f:tr_y.append(line[:-1].split(' '))
-		else:
-			tr_y = Parallel(n_jobs=args.n_jobs)(delayed(read_text)(str(file),target=args.target) for file in tqdm(todo))
-		tr_y, encode_table = encode_target(tr_y, table=encode_table, mode=args.target, max_idx=args.n_tokens)
+	output_dir = os.path.join(args.output_path,'_'.join(['mosi',str(args.feature_type)+str(dim)]))
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
 
-		if output_dir is None:
-			output_dir = os.path.join(args.output_path,'_'.join(['libri',str(args.feature_type)+str(dim),str(args.target)+str(len(encode_table))]))
-		if not os.path.exists(output_dir): os.makedirs(output_dir)
-		cur_path = os.path.join(output_dir,s)
-		if not os.path.exists(cur_path): os.makedirs(cur_path)
+	print('Extracting acoustic feature...',flush=True)
+	tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_feature)(str(file), feature=args.feature_type, cmvn=args.apply_cmvn, \
+										save_feature=os.path.join(output_dir, str(file).split('/')[-1].replace('.wav',''))) for file in tqdm(todo))
 
-		print('Extracting acoustic feature...',flush=True)
-		tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_feature)(str(file), feature=args.feature_type, cmvn=args.apply_cmvn, \
-											save_feature=os.path.join(cur_path, str(file).split('/')[-1].replace('.flac',''))) for file in tqdm(todo))
+	# Loading labels
+	DATASET = md.cmu_mosi
+	try:
+		md.mmdataset(DATASET.labels, args.data_path)
+	except RuntimeError:
+		print("Labels have been downloaded previously.")
 
-		# sort by len
-		sorted_y = ['_'.join([str(i) for i in tr_y[idx]]) for idx in reversed(np.argsort(tr_x))]
-		sorted_todo = [os.path.join(s,str(todo[idx]).split('/')[-1].replace('.flac','.npy')) for idx in reversed(np.argsort(tr_x))]
-		# Dump label
-		df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo],'length':list(reversed(sorted(tr_x))),'label':sorted_y})
-		df.to_csv(os.path.join(output_dir,s+'.csv'))
+	label_field = 'CMU_MOSI_Opinion_Labels'
+	features = [
+		label_field,
+	]
 
-		with open(os.path.join(output_dir,"mapping.pkl"), "wb") as fp:
-			pickle.dump(encode_table, fp)
+	recipe = {feat: os.path.join(args.data_path, feat) + '.csd' for feat in features}
+	dataset = md.mmdataset(recipe)
+	dataset.align(label_field)
+
+	# Check each label has corresponding utterance
+	utterances = os.listdir(output_dir)
+	for segment_sdk in dataset[label_field].keys():
+		segment_npy = sdk2npy(segment_sdk)
+		try:
+			assert segment_npy in utterances
+		except AssertionError:
+			print('AssertionError: Cannot find corresponding utterance for given label')
+
+		# Check the inverse is itself by npy2sdk
+		try:
+			assert npy2sdk(segment_npy) == segment_sdk
+		except AssertionError:
+			print('AssertionError: npt2sdk funtion has bug')
+
+	# sort by len
+	sorted_xlen = []
+	sorted_y = []
+	sorted_todo = []
+	for idx in reversed(np.argsort(tr_x)):
+		filename = str(todo[idx]).split('/')[-1].replace('.wav','.npy')
+		sdkname = npy2sdk(filename)
+		if sdkname in dataset[label_field].keys():
+			sorted_xlen.append(tr_x[idx])
+			sorted_y.append(dataset[label_field][sdkname]['features'].reshape(-1))
+			sorted_todo.append(filename)
+
+	# Dump label
+	df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo],'length':list(sorted_xlen),'label':sorted_y})
+	df.to_csv(os.path.join(output_dir,'mosi.csv'))
 
 	print('All done, saved at', output_dir, 'exit.')
 
@@ -100,30 +140,12 @@ def acoustic_preprocess(args, tr_set, encode_table, output_dir, bpe_dir, dim):
 # MAIN #
 ########
 def main():
-
 	# get arguments
 	args = get_preprocess_args()
-
-	# Process data
-	sets = ['train-clean-100','train-clean-360','train-other-500','dev-clean','dev-other','test-clean','test-other']
-	encode_table = None
-	output_dir = None
 	dim = num_freq if args.feature_type == 'linear' else mel_dim
 
-	# BPE training
-	if args.target == 'subword':
-		encode_table, output_dir, bpe_dir = subword_preprocess(args, sets, encode_table, output_dir, dim)
-
-	# Select data sets
-	print('')
-	print('Data sets :')
-	for idx,s in enumerate(sets):
-		print('\t',idx,':',s)
-	tr_set = input('Please enter the index of splits you wish to use preprocess. (seperate with space): ')
-	tr_set = [sets[int(t)] for t in tr_set.split(' ')]
-
-	# Acoustic Feature Extraction & Make Date Table
-	acoustic_preprocess(args, tr_set, encode_table, output_dir, bpe_dir, dim)
+	# Acoustic Feature Extraction
+	acoustic_preprocess(args, dim)
 
 
 if __name__ == '__main__':
