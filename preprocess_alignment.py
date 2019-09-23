939f30a7e43650d5ec84d188464c189ffbd5c6f5
andi611 <tingweiandyliu@gmail.com>
2019-09-23 02:17:39 +0200

    fix phone preprocessing

--- preprocess_alignment.py
+++ preprocess_alignment.py
@@ -52,7 +52,7 @@ def phone_preprocess(data_path, output_path, sets, unaligned):
 		print('Computing', s, 'data...')
 		for path in tqdm(list(Path(os.path.join(data_path, s)).rglob("*.txt"))):
 			check_name = path.as_posix().split('/')[-1].split('.')[0]
-			if check_name not in unaligned and check_name != 'unaligned.txt':
+			if check_name not in unaligned and check_name != 'unaligned':
 				for line in open(path).readlines():
 					phone = line.strip('\n').split(' ')[-1]
 					if phone not in phone2idx:
@@ -75,7 +75,7 @@ def phone_preprocess(data_path, output_path, sets, unaligned):
 		print('Preprocessing phone alignments...', flush=True)
 		for path in tqdm(todo):
 			check_name = path.as_posix().split('/')[-1].split('.')[0]
-			if check_name not in unaligned and check_name != 'unaligned.txt':
+			if check_name not in unaligned and check_name != 'unaligned':
 				x = []
 				file = open(path).readlines()
 				for line in file:
