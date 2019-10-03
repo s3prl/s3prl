eeeaac07f7bdceebf1b4f3fbfbebeea24ed1f86a
pohanchi <aapp1420@gmail.com>
2019-10-03 10:19:27 +0800

    add order process

--- utils/mam.py
+++ utils/mam.py
@@ -141,3 +141,78 @@ def process_test_MAM_data(spec):
         attn_mask = torch.FloatTensor(attn_mask).to(dtype=torch.float32)
 
     return spec_stacked, pos_enc, attn_mask # (x, pos_enc, attention_mask)
+
+
+def process_train_MAM_data_spectrum_order(spec):
+    """Process training data for the masked acoustic model"""
+    with torch.no_grad():
+        if len(spec) == 2: # if self.duo_feature: dataloader will output `source_spec` and `target_spec`
+            source_spec = spec[0]
+            target_spec = spec[1]
+        elif len(spec) == 1:
+            source_spec = spec[0]
+            target_spec = copy.deepcopy(spec[0])
+        else:
+            raise NotImplementedError('Input spec sould be either (spec,) or (target_spec, source_spec), where `spec` has shape BxTxD.')
+
+        # Down sample
+        spec_masked = down_sample_frames(source_spec) # (batch_size, seq_len, mel_dim * dr)
+        spec_stacked = down_sample_frames(target_spec) # (batch_size, seq_len, mel_dim * dr)
+        assert(spec_masked.shape[1] == spec_stacked.shape[1]), 'Input and output spectrogram should have the same shape'
+
+        # Record length for each uttr
+        spec_len = np.sum(np.sum(spec_stacked.data.numpy(), axis=-1) != 0, axis=-1)
+        spec_len = [int(sl) for sl in spec_len]
+
+        batch_size = spec_stacked.shape[0]
+        seq_len = spec_stacked.shape[1]
+
+        pos_enc = position_encoding(seq_len, batch_size) # (batch_size, seq_len, hidden_size)
+        mask_label = np.zeros_like(spec_stacked)
+        attn_mask = np.ones((batch_size, seq_len)) # (batch_size, seq_len)
+
+        order_label = torch.zeros(batch_size,1)
+        
+
+        for idx in range(len(spec_stacked)):
+
+            # ignore middle 20 % samples and divide the spec to two part to the second task spec order prediction
+            sample_length = int(spec_len[idx])
+            first_proportion  = spec_stacked[idx][: int(0.4*sample_length)]
+            second_proportion = spec_stacked[idx][int(0.6*sample_length): ]
+            ignore_part = spec_stacked[idx][int(0.4*sample_length): int(0.6*sample_length) ]
+
+            if random.uniform(0, 1) > 0.5:
+                order_label[idx] = 1
+            else:
+                order_label[idx] = 0
+                spec_stacked[idx] = torch.cat([second_proportion,ignore_part,first_proportion],dim=0)
+
+            chose_proportion = int(spec_len[idx]*mask_proportion) # chooses % of the frame positions at random for prediction
+            sub_mask_proportion = int(chose_proportion*0.8) # replace the i-th frame with (1) the [MASK] frame 80% of the time
+            sub_rand_proportion = int(chose_proportion*0.1) # a random frame 10% of the time
+
+            
+            sample_index = random.sample(range(spec_len[idx]), chose_proportion + sub_rand_proportion) # sample the chosen_index and random frames
+            chosen_index = sample_index[:chose_proportion]
+            masked_index = chosen_index[:sub_mask_proportion]
+
+            if sub_rand_proportion > 0:
+                random_index = chosen_index[-sub_rand_proportion:]
+                random_frames = sample_index[-sub_rand_proportion:]
+                spec_masked[idx][random_index] = spec_masked[idx][random_frames]
+            
+            spec_masked[idx][masked_index] = 0 # mask frames to zero
+            mask_label[idx][chosen_index] = 1 # the frames where gradients will be calculated on 
+
+            # zero vectors for padding dimension
+            pos_enc[idx][spec_len[idx]:] = 0  
+            attn_mask[idx][spec_len[idx]:] = 0
+
+        spec_masked = spec_masked.to(dtype=torch.float32)
+        pos_enc = torch.FloatTensor(pos_enc).to(dtype=torch.float32)
+        mask_label = torch.ByteTensor(mask_label).to(dtype=torch.uint8)
+        attn_mask = torch.FloatTensor(attn_mask).to(dtype=torch.float32)
+        spec_stacked = spec_stacked.to(dtype=torch.float32)
+
+    return spec_masked, pos_enc, mask_label, attn_mask, spec_stacked, order_label
