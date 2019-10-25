07555bd18f9d7f80841ab813cb980972e4023451
Leo Yang <leo19941227@gmail.com>
2019-10-25 15:17:20 +0800

    organize observe code

--- observe/observe_weights.py
+++ observe/observe_weights.py
@@ -3,10 +3,12 @@ import torch
 from utils import timer
 import matplotlib.pyplot as plt
 
-
 ckpt_path = sys.argv[1]
+imgname = sys.argv[2]
+
 ckpt = torch.load(ckpt_path)
 weights = ckpt['Classifier']['weight']
 norm = weights.abs() / weights.abs().sum()
 plt.plot(norm.cpu().numpy())
-plt.savefig('weights.png')
+plt.savefig(imgname)
+
