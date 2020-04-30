df8583f49db27e18d8e5abc2a0970083ee347a07
andi611 <tingweiandyliu@gmail.com>
2020-04-30 11:17:28 +0800

    fix bug

--- example_extract_finetune.py
+++ example_extract_finetune.py
@@ -21,23 +21,24 @@ from downstream.solver import get_mockingjay_optimizer
 
 # setup the mockingjay model
 options = {
-    'ckpt_file' : 'result/result_mockingjay/mockingjay_libri_sd1337_MelBase/mockingjay-500000.ckpt',
+    'ckpt_file'     : 'result/result_mockingjay/mockingjay_libri_sd1337_MelBase/mockingjay-500000.ckpt',
     'load_pretrain' : 'True',
-    'no_grad' : 'False',
-    'dropout' : 'default'
+    'no_grad'       : 'False',
+    'dropout'       : 'default',
+    'spec_aug'      : 'False',
 }
-model = MOCKINGJAY(options=options, inp_dim=160)
+mockingjay = MOCKINGJAY(options=options, inp_dim=160)
 
 # setup your downstream class model
 classifier = example_classifier(input_dim=768, hidden_dim=128, class_num=2).cuda()
 
 # construct the Mockingjay optimizer
-params = list(model.named_parameters()) + list(classifier.named_parameters())
+params = list(mockingjay.named_parameters()) + list(classifier.named_parameters())
 optimizer = get_mockingjay_optimizer(params=params, lr=4e-3, warmup_proportion=0.7, training_steps=50000)
 
 # forward
 example_inputs = torch.zeros(1200, 3, 160) # A batch of spectrograms: (time_step, batch_size, dimension)
-reps = model(example_inputs) # returns: (time_step, batch_size, hidden_size)
+reps = mockingjay(example_inputs) # returns: (time_step, batch_size, hidden_size)
 reps = reps.permute(1, 0, 2) # change to: (batch_size, time_step, feature_size)
 labels = torch.LongTensor([0, 1, 0]).cuda()
 loss = classifier(reps, labels)
@@ -48,5 +49,5 @@ optimizer.step()
 
 # save
 PATH_TO_SAVE_YOUR_MODEL = 'example.ckpt'
-states = {'Classifier': classifier.state_dict(), 'Mockingjay': model.state_dict()}
+states = {'Classifier': classifier.state_dict(), 'Mockingjay': mockingjay.model.state_dict()}
 torch.save(states, PATH_TO_SAVE_YOUR_MODEL)
