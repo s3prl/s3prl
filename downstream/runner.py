b4364b9000bd75a6c2720f5638de806045a05748
andi611 <tingweiandyliu@gmail.com>
2020-06-13 22:56:46 +0800

    add downstream scripts

---
+++ downstream/runner.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ downstream/runner.py ]
+#   Synopsis     [ runner to perform downstream train/dev/test ]
+#   Author       [ Andy T. Liu (Andi611) ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+import os
+import math
+import torch
+import random
+from tqdm import tqdm
+from torch.optim import Adam
+from tensorboardX import SummaryWriter
+from downstream.solver import get_optimizer
+
+
+##########
+# RUNNER #
+##########
+class Runner():
+    ''' Handler for complete training and evaluation progress'''
+    def __init__(self, args, runner_config, dataloader, upstream, downstream, expdir):
+
+        self.device = torch.device('cuda') if (args.gpu and torch.cuda.is_available()) else torch.device('cpu')
+        self.model_kept = []
+        self.global_step = 1
+        self.log = SummaryWriter(expdir)
+
+        self.args = args
+        self.config = runner_config
+        self.dataloader = dataloader
+        self.upstream_model = upstream.to(self.device)
+        self.downstream_model = downstream.to(self.device)
+        self.expdir = expdir
+
+
+    def set_model(self):
+
+        if self.args.fine_tune:
+            # Setup Fine tune optimizer
+            self.upstream_model.train()
+            param_optimizer = list(self.upstream_model.named_parameters()) + list(self.downstream_model.named_parameters())
+            self.optimizer = get_optimizer(params=param_optimizer,
+                                           lr=float(self.config['learning_rate']), 
+                                           warmup_proportion=float(self.config['warmup_proportion']),
+                                           training_steps=int(self.config['total_steps']))
+        else:
+            self.upstream_model.eval()
+            self.optimizer = Adam(self.downstream_model.parameters(), lr=float(self.config['learning_rate']), betas=(0.9, 0.999))
+        
+        self.downstream_model.train()
+
+
+    def save_model(self, name='states'):
+        
+        all_states = {
+            'Upstream': self.upstream_model.state_dict() if self.args.fine_tune else None,
+            'Downstream': self.downstream_model.state_dict(),
+            'Optimizer': self.optimizer.state_dict(),
+            'Global_step': self.global_step,
+            'Settings': {
+                'Config': self.config,
+                'Paras': self.args,
+            },
+        }
+
+        model_path = '{}/{}-{}.ckpt'.format(self.expdir, name, self.global_step)
+        torch.save(all_states, model_path)
+        self.model_kept.append(model_path)
+
+        if len(self.model_kept) >= int(self.config['max_keep']):
+            os.remove(self.model_kept[0])
+            self.model_kept.pop(0)
+
+
+    def train(self):
+        ''' Training of downstream tasks'''
+
+        pbar = tqdm(total=int(self.config['total_steps']))
+        corrects = 0
+        valids = 0
+        best_acc = 0.0
+        best_eval_acc = 0.0
+        best_test_acc = 0.0
+        loses = 0.0
+
+        while self.global_step <= int(self.config['total_steps']):
+
+            for features, labels in tqdm(self.dataloader['train'], desc="Iteration"):
+                try:
+                    if self.global_step > int(self.config['total_steps']): break
+                    
+                    # features: (1, batch_size, seq_len, feature)
+                    # dimension of labels depend on task and dataset, but the first dimention is always trivial due to bucketing, eg. (1, ...)
+                    features = features.squeeze(0).to(device=self.device, dtype=torch.float32)
+                    features = self.upstream_model(features)
+
+                    # Since zero padding technique, some timestamps of features are not valid
+                    # For each timestamps, we mark 1 on valid timestamps, and 0 otherwise
+                    # This variable can be useful for frame-wise metric, like phoneme recognition or speaker verification
+                    # label_mask: (batch_size, seq_len), LongTensor
+                    # valid_lengths: (batch_size), LongTensor
+                    labels = labels.squeeze(0).to(device=self.device)  # labels can be torch.long or torch.float (regression)
+                    label_mask = (features.sum(dim=-1) != 0).type(torch.LongTensor).to(device=self.device, dtype=torch.long)
+                    valid_lengths = label_mask.sum(dim=1)
+
+                    if 'utterance' in self.args.run:
+                        # labels: (batch_size, )
+                        loss, _, correct, valid = self.downstream_model(features, labels, valid_lengths)
+                    else:
+                        # labels: (batch_size, seq_len)
+                        loss, _, correct, valid = self.downstream_model(features, labels, label_mask)
+
+                    # Accumulate Loss
+                    loss.backward()
+
+                    # record
+                    loses += loss.detach().item()
+                    corrects += correct
+                    valids += valid
+
+                    # gradient clipping
+                    if self.args.fine_tune: 
+                        grad_norm = torch.nn.utils.clip_grad_norm_(list(self.upstream_model.parameters()) + list(self.downstream_model.parameters()), \
+                                                                   float(self.config['gradient_clipping']))
+                    else:
+                        grad_norm = torch.nn.utils.clip_grad_norm_(self.downstream_model.parameters(), \
+                                                                   float(self.config['gradient_clipping']))
+                    
+                    # step
+                    if math.isnan(grad_norm):
+                        print('[Runner] - Error : grad norm is NaN @ step ' + str(self.global_step))
+                    else:
+                        self.optimizer.step()
+                    self.optimizer.zero_grad()
+
+                    # logging
+                    if self.global_step % int(self.config['log_step']) == 0:
+                        # Log
+                        acc = corrects.item() / valids.item()
+                        los = loses / int(self.config['log_step'])
+                        self.log.add_scalar('acc', acc, self.global_step)
+                        self.log.add_scalar('loss', los, self.global_step)
+                        self.log.add_scalar('gradient norm', grad_norm, self.global_step)
+                        pbar.set_description('Loss %.5f, Acc %.5f' % (los, acc))
+
+                        loses = 0.0
+                        corrects = 0
+                        valids = 0
+
+                    if self.global_step % int(self.config['save_step']) == 0 and acc > best_acc:
+                        self.save_model()
+                        best_acc = acc
+
+                    # evaluate on the self.config['evaluation'] set
+                    if self.global_step % int(self.config['dev_step']) == 0:
+                        print('[Runner] - Evaluating on: ', self.config['evaluation'])
+                        torch.cuda.empty_cache()
+                        eval_loss, eval_acc, _ = self.evaluate(split=self.config['evaluation'])
+                        self.log.add_scalar(f"{self.config['evaluation']}_loss", eval_loss, self.global_step)
+                        self.log.add_scalar(f"{self.config['evaluation']}_acc", eval_acc, self.global_step)
+                        if eval_acc > best_eval_acc:
+                            print('[Runner] - Saving new best model on: ', self.config['evaluation'])
+                            self.save_model(name=f"best_{self.config['evaluation']}")
+                            torch.cuda.empty_cache()
+                            best_eval_acc = eval_acc
+                        
+                        # evaluate on the test set if not already
+                        if self.config['evaluation'] != 'test':
+                            torch.cuda.empty_cache()
+                            test_loss, test_acc, _ = self.evaluate(split='test')
+                            self.log.add_scalar('test_loss', test_loss, self.global_step)
+                            self.log.add_scalar('test_acc', test_acc, self.global_step)
+                            if test_acc > best_test_acc:
+                                print('[Runner] - Saving new best model on: ', 'test')
+                                self.save_model(name='best_test')
+                                torch.cuda.empty_cache()
+                                best_test_acc = test_acc
+
+                except RuntimeError as e:
+                    if 'CUDA out of memory' in str(e):
+                        print('[Runner] - CUDA out of memory at step: ', self.global_step)
+                        torch.cuda.empty_cache()
+                        self.optimizer.zero_grad()
+                    else:
+                        raise
+
+                pbar.update(1)
+                self.global_step += 1
+                
+        pbar.close()
+        self.log.close()
+
+
+    def evaluate(self, split):
+        ''' Testing of downstream tasks'''
+
+        self.upstream_model.eval()
+        self.downstream_model.eval()
+        
+        valid_count = 0
+        correct_count = 0
+        loss_sum = 0
+        all_logits = []
+
+        oom_counter = 0
+        for features, labels in tqdm(self.dataloader[split], desc="Iteration"):
+            with torch.no_grad():
+                try:
+                    # features: (1, batch_size, seq_len, feature)
+                    # dimension of labels depend on task and dataset, but the first dimention is always trivial due to bucketing, eg. (1, ...)
+                    features = features.squeeze(0).to(device=self.device, dtype=torch.float32)
+                    features = self.upstream_model(features)
+
+                    # Since zero padding technique, some timestamps of features are not valid
+                    # For each timestamps, we mark 1 on valid timestamps, and 0 otherwise
+                    # This variable can be useful for frame-wise metric, like phoneme recognition or speaker verification
+                    # label_mask: (batch_size, seq_len), LongTensor
+                    labels = labels.squeeze(0).to(device=self.device)
+                    label_mask = (features.sum(dim=-1) != 0).type(torch.LongTensor).to(device=self.device, dtype=torch.long)
+                    valid_lengths = label_mask.sum(dim=1)
+
+                    if 'utterance' in self.args.run:
+                        # labels: (batch_size, )
+                        loss, logits, correct, valid = self.downstream_model(features, labels, valid_lengths)
+                    else:
+                        # labels: (batch_size, seq_len)
+                        loss, logits, correct, valid = self.downstream_model(features, labels, label_mask)
+                    
+                    loss_sum += loss.detach().cpu().item()
+                    all_logits.append(logits)
+                    correct_count += correct.item()
+                    valid_count += valid.item()
+
+                except RuntimeError as e:
+                    if 'CUDA out of memory' in str(e):
+                        if oom_counter >= 10: 
+                            oom_counter = 0
+                            break
+                        else:
+                            oom_counter += 1
+                        print('[Runner] - CUDA out of memory during testing, aborting after ' + str(10 - oom_counter) + ' more tries...')
+                        torch.cuda.empty_cache()
+                    else:
+                        raise
+
+        average_loss = loss_sum / len(self.dataloader[split])
+        eval_acc = correct_count * 1.0 / valid_count
+        print(f'[Runner] - Test result: loss {average_loss}, acc {eval_acc}')
+        
+        self.upstream_model.train()
+        self.downstream_model.train()
+
+        return average_loss, eval_acc, all_logits
---
+++ run_downstream.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ run_downstream.py ]
+#   Synopsis     [ scripts for running downstream evaluation of upstream models ]
+#   Author       [ Andy T. Liu (Andi611) ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+import os
+import yaml
+import torch
+import random
+import argparse
+import numpy as np
+from shutil import copyfile
+from dataloader import get_Dataloader
+from transformer.nn_transformer import TRANSFORMER
+from downstream.model import dummy_upstream, LinearClassifier, RnnClassifier
+from downstream.runner import Runner
+
+
+"""
+USAGE:
+    You can easily initialize your upstream pre-trained model in the function `get_upstream_model()`.
+    There are only three simple requirements for each upstream model:
+        1) Implement the `forward` method of `nn.Module`,
+        2) Contains the `out_dim` attribute.
+        3) Takes input and output in the shape of: (batch_size, time_steps, feature_dim)
+    then you can use these scipts to evaluate your model.
+"""
+
+
+########################
+# DOWNSTREAM ARGUMENTS #
+########################
+def get_downstream_args():
+    
+    parser = argparse.ArgumentParser(description='Argument Parser for the S3PLR project.')
+    
+    # required
+    parser.add_argument('--run',  choices=['phone_linear', 'phone_1hidden', 'phone_concat', 'speaker_frame', 'speaker_utterance'], help='select task.', required=True)
+
+    # upstream settings
+    parser.add_argument('--ckpt', default='', type=str, help='path to upstream pre-trained checkpoint, required if using other than baseline', required=False)
+    parser.add_argument('--upstream', choices=['transformer', 'apc', 'baseline'], default='baseline', help='whether to use upstream models for speech representation or fine-tune.', required=False)
+    parser.add_argument('--input_dim', default=80, type=str, help='input dimension used to initialize transformer models', required=False)
+    parser.add_argument('--fine_tune', action='store_true', help='whether to fine tune the transformer model with downstream task.', required=False)
+    parser.add_argument('--weighted_sum', action='store_true', help='whether to use weighted sum on the transformer model with downstream task.', required=False)
+    
+    # Options
+    parser.add_argument('--name', default=None, type=str, help='Name of current experiment.', required=False)
+    parser.add_argument('--config', default='config/downstream.yaml', type=str, help='Path to downstream experiment config.', required=False)
+    parser.add_argument('--expdir', default='', type=str, help='path to store experiment result, if empty then default is used', required=False)
+    parser.add_argument('--seed', default=1337, type=int, help='Random seed for reproducable results.', required=False)
+    parser.add_argument('--cpu', action='store_true', help='Disable GPU training.')
+
+    # parse
+    args = parser.parse_args()
+    setattr(args, 'gpu', not args.cpu)
+    setattr(args, 'task', 'cpc_phone' if 'phone' in args.run else 'speaker')
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    
+    return args, config
+
+
+##################
+# GET DATALOADER #
+##################
+def get_dataloader(args, dataloader_config):
+    
+    print('[run_downstream] - Loading input data: ' + str(dataloader_config['train_set']) + ' from ' + dataloader_config['data_path'])
+    if args.task == 'speaker':
+        print('[run_downstream] - Loading speaker data: ' + str(dataloader_config['train_set']) + ' from ' + dataloader_config['data_path'])
+    else:
+        print('[run_downstream] - Loading phone data: ' + dataloader_config['phone_path'])
+
+    print('[run_downstream] - getting train dataloader...')
+    train_loader = get_Dataloader(split='train', load=args.task, use_gpu=args.gpu, **dataloader_config)
+
+    print('[run_downstream] - getting dev dataloader...')
+    dev_loader = get_Dataloader(split='dev', load=args.task, use_gpu=args.gpu, **dataloader_config)
+
+    print('[run_downstream] - getting test dataloader...')
+    test_loader = get_Dataloader(split='test', load=args.task, use_gpu=args.gpu, **dataloader_config)
+    
+    return train_loader, dev_loader, test_loader
+
+
+################
+# GET UPSTREAM #
+################
+"""
+Upstream model should meet the requirement of:
+    1) Implement the `forward` method of `nn.Module`,
+    2) Contains the `out_dim` attribute.
+    3) Takes input and output in the shape of: (batch_size, time_steps, feature_dim)
+"""
+def get_upstream_model(args):
+    
+    print('[run_downstream] - getting upstream model:', args.upstream)
+
+    if args.upstream == 'transformer':
+        options = {'ckpt_file'     : args.ckpt,
+                   'load_pretrain' : 'True',
+                   'no_grad'       : 'True' if not args.fine_tune else 'False',
+                   'dropout'       : 'default',
+                   'spec_aug'      : 'False',
+                   'spec_aug_prev' : 'True',
+                   'weighted_sum'  : 'True' if args.weighted_sum else 'False',
+                   'select_layer'  : -1,
+        }
+        upstream_model = TRANSFORMER(options, args.input_dim)
+        upstream_model.permute_input = False
+        
+    elif args.upstream == 'apc':
+        raise NotImplementedError
+
+    elif args.upstream == 'baseline':
+        upstream_model = dummy_upstream(args.input_dim)
+
+    else:
+        raise NotImplementedError ######### plug in your upstream pre-trained model here #########
+
+    assert(hasattr(upstream_model, 'forward'))
+    assert(hasattr(upstream_model, 'out_dim'))
+    return upstream_model
+
+
+##################
+# GET DOWNSTREAM #
+##################
+def get_downstream_model(args, input_dim, class_num, config):
+    
+    model_name = args.run.split('_')[-1].replace('frame', 'linear') # support names: ['linear', '1hidden', 'concat', 'utterance']
+    model_config = config['model'][model_name]
+
+    if args.task == 'speaker' and 'utterance' in args.run:
+        downstream_model = RnnClassifier(input_dim, class_num, model_config)
+    else:
+        downstream_model = LinearClassifier(input_dim, class_num, model_config)
+    
+    return downstream_model
+
+
+########
+# MAIN #
+########
+def main():
+    
+    # get config and arguments
+    args, config = get_downstream_args()
+    
+    # Fix seed and make backends deterministic
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available(): torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+    # mkdir
+    if args.expdir == '':
+        if args.name is None: args.name = 'exp_' + str(random.randint(0, 999))
+        expdir = os.path.join('result/result_' + args.upstream + '_' + args.task + '/', args.name)
+    else:
+        expdir = args.expdir
+    if not os.path.exists(expdir):
+        os.makedirs(expdir)
+    copyfile(args.config, os.path.join(expdir, args.config.split('/')[-1]))
+
+    # get dataloaders
+    train_loader, dev_loader, test_loader = get_dataloader(args, config['dataloader'])
+
+    # get upstream model
+    upstream_model = get_upstream_model(args) ######### plug in your upstream pre-trained model here #########
+
+    # get downstream model
+    downstream_model = get_downstream_model(args, upstream_model.out_dim, train_loader.dataset.class_num, config)
+
+    runner = Runner(args=args,
+                    runner_config=config['runner'],
+                    dataloader= {'train':train_loader, 'dev':dev_loader, 'test':test_loader}, 
+                    upstream=upstream_model, 
+                    downstream=downstream_model, 
+                    expdir=expdir)
+    
+    runner.set_model()
+    runner.train()
+
+
+if __name__ == '__main__':
+    main()
