032e696168d1ce076929ec4b8be14258cf7b568a
andi611 <tingweiandyliu@gmail.com>
2020-06-27 12:33:53 +0800

    update preprocessing

--- src/kaldi_egs_librispeech_s5/dump_fbank_cmvn.sh
+++ src/kaldi_egs_librispeech_s5/dump_fbank_cmvn.sh
@@ -6,7 +6,7 @@ mkdir $data_root/fbank_cmvn/
 
 for part in dev_clean test_clean train_clean_100 train_clean_360 train_other_500; do
   mkdir $data_root/fbank_cmvn/$part/
-  apply-cmvn --utt2spk=ark:$data_root/data/$part/utt2spk  ark:$data_root/fbank/cmvn_$part.ark scp:$data_root/data/$part/feats.scp ark:- | add-deltas --delta-order=0 ark:- ark:$data_root/fbank_cmvn/$part/fbank_cmvn.ark
+  apply-cmvn --utt2spk=ark:$data_root/data/$part/utt2spk scp:$data_root/fbank/cmvn_$part.scp scp:$data_root/data/$part/feats.scp ark,scp:$data_root/fbank_cmvn/$part/fbank_cmvn.ark,$data_root/fbank_cmvn/$part/fbank_cmvn.scp
 done
 
 du -sh $data_root/fbank_cmvn/*
--- src/kaldi_egs_librispeech_s5/dump_mfcc_cmvn.sh
+++ src/kaldi_egs_librispeech_s5/dump_mfcc_cmvn.sh
@@ -6,7 +6,7 @@ mkdir $data_root/mfcc_cmvn/
 
 for part in dev_clean test_clean train_clean_100 train_clean_360 train_other_500; do
   mkdir $data_root/mfcc_cmvn/$part/
-  apply-cmvn --utt2spk=ark:$data_root/data/$part/utt2spk  ark:$data_root/mfcc/cmvn_$part.ark scp:$data_root/data/$part/feats.scp ark:- | add-deltas --delta-order=2 ark:- ark:$data_root/mfcc_cmvn/$part/mfcc_cmvn.ark
+  apply-cmvn --utt2spk=ark:$data_root/data/$part/utt2spk scp:$data_root/mfcc/cmvn_$part.scp scp:$data_root/data/$part/feats.scp ark,scp:$data_root/mfcc_cmvn/$part/mfcc_cmvn_nd.ark,$data_root/mfcc_cmvn/$part/mfcc_cmvn_nd.scp | add-deltas --delta-order=2 ark:$data_root/mfcc_cmvn/$part/mfcc_cmvn_nd.ark ark:$data_root/mfcc_cmvn/$part/mfcc_cmvn.ark
 done
 
 du -sh $data_root/mfcc_cmvn/*
