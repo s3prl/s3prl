46aa9472e185c6f6506d49d91d78462172c9663d
Leo Yang <leo19941227@gmail.com>
2020-10-07 01:48:42 +0800

    fucking bug

--- small_models.py
+++ small_models.py
@@ -11,7 +11,7 @@ class SmallModelWrapper(nn.Module):
 
     def forward(self, feats_inp, linears_inp, linears_tar):
         predicted, _ = self.model(features=feats_inp, linears=linears_inp)
-        loss, _ = self.objective(predicted, linears_tar, feats_inp.sum(dim=-1) > 0)
+        loss, _ = self.objective(predicted, linears_tar, feats_inp.sum(dim=-1) != 0)
         return loss, predicted
 
 
@@ -88,3 +88,29 @@ class Residual(nn.Module):
         offset = self.scaling_layer(offset)
         predicted = linears * offset
         return predicted, {'offset': offset}
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=201, output_size=201, hidden_size=201, num_layers=3, bidirectional=False,
+                 activation='ReLU', **kwargs):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True, bidirectional=bidirectional)
+        self.scaling_layer = nn.Sequential(
+            nn.Linear(max(1, int(bidirectional) * 2) * hidden_size, output_size), eval(f'nn.{activation}()'))
+        self.init_weights()
+        self.bidirectional = bidirectional
+
+    def init_weights(self):
+        for name, param in self.named_parameters():
+            if 'weight_ih' in name or 'scaling_layer.0.weight' in name:
+                nn.init.xavier_uniform_(param.data)
+            elif 'weight_hh' in name:
+                nn.init.orthogonal_(param.data)
+            elif 'bias' in name:
+                nn.init.constant_(param.data, 0)
+
+    def forward(self, features, **kwargs):
+        predicted, _ = self.lstm(features)
+        predicted = self.scaling_layer(predicted)
+        return predicted, {}
