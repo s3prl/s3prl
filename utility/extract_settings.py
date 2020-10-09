9f6d2ad74329ff0e6a6419cb1eaa0821597bb8c5
Leo Yang <leo19941227@gmail.com>
2020-10-09 12:58:41 +0800

    update extract and overwrite

--- utility/extract_settings.py
+++ utility/extract_settings.py
@@ -14,3 +14,7 @@ with open(f'{save_dir}/args.tmp.yaml', 'w') as handle:
 with open(f'{save_dir}/config.tmp.yaml', 'w') as handle:
     config = ckpt['Settings']['Config']
     yaml.dump(config, handle)
+
+with open(f'{save_dir}/global_step.tmp.txt', 'w') as handle:
+    step = ckpt['Global_step']
+    handle.write(f'{step}\n')
--- utility/overwrite_settings.py
+++ utility/overwrite_settings.py
@@ -6,7 +6,8 @@ from argparse import Namespace
 ckpt_pth = sys.argv[1]
 args_pth = sys.argv[2]
 config_pth = sys.argv[3]
-new_ckpt_pth = sys.argv[4]
+global_step_pth = sys.argv[4]
+new_ckpt_pth = sys.argv[5]
 
 ckpt = torch.load(ckpt_pth, map_location='cpu')
 
@@ -19,5 +20,10 @@ with open(args_pth, 'r') as handle:
 with open(config_pth, 'r') as handle:
     config = yaml.load(handle, Loader=yaml.FullLoader)
     ckpt['Settings']['Config'] = config
+
+# overwrite config
+with open(global_step_pth, 'r') as handle:
+    step = int(handle.readline()[:-1])
+    ckpt['Global_step'] = step
 
 torch.save(ckpt, new_ckpt_pth)
