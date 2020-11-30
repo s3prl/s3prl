3ef431b736e42fb271bd36243c63009ce6b4c504
pohanchi <aapp1420@gmail.com>
2020-11-30 22:57:19 +0800

    verification task finish

--- benchmark/downstream/speaker_verifi/dataset.py
+++ benchmark/downstream/speaker_verifi/dataset.py
@@ -33,151 +33,23 @@ from torch.utils.data import Dataset
 from librosa.util import find_files
 
 
-# Voxceleb 1 + 2 Speaker Verification
-# class Voxceleb_Dataset(Dataset):
-#     def __init__(self, file_path, max_timestep=500, meta_data=None, dev_spkr_ids=None, utterance_sample=5, is_dev=False, is_eval=False):
-
-#         # Read file
-#         self.root = file_path
-#         self.all_speakers  = [f.path for f in os.scandir(file_path) if f.is_dir()]
-#         # Crop seqs that are too long
-#         self.max_timestep = max_timestep
-#         self.utterance_sample = utterance_sample
-#         self.is_eval = is_eval
-#         self.meta_data =meta_data
-#         self.pair_table = None
-#         self.is_dev = is_dev
-
-#         if not self.is_dev and not self.is_eval:
-#             self.speakers  = [path for path in self.all_speakers if path.split("/")[-1] not in dev_spkr_ids]
-#             self.speaker_num = len(self.speakers)
-#             self.number_range = list(range(self.speaker_num))
-        
-#         if self.is_eval:
-#             self.pair_table = []
-#             usage_list = open(self.meta_data, "r").readlines()
-#             for pair in usage_list:
-#                 list_pair = pair.split()
-#                 pair_1= os.path.join(self.root, list_pair[1])
-#                 pair_2= os.path.join(self.root, list_pair[2])
-#                 one_pair = [list_pair[0],pair_1,pair_2 ]
-#                 self.pair_table.append(one_pair)
-
-#         if self.is_dev:
-#             self.pair_table = []
-#             usage_list = open(self.meta_data, "r").readlines()
-#             for pair in usage_list:
-#                 list_pair = pair.split()
-#                 pair_1= os.path.join(self.root, list_pair[1])
-#                 pair_2= os.path.join(self.root, list_pair[2])
-#                 one_pair = [list_pair[0],pair_1,pair_2 ]
-#                 self.pair_table.append(one_pair)
-
-            
-
-#     def __len__(self):
-#         if self.is_eval or self.is_dev:
-#             return len(self.pair_table)
-#         else:
-#             return len(self.speakers)
-    
-#     def __getitem__(self, idx):
-#         if self.is_eval or self.is_dev:
-#             y_label, x1_path, x2_path = self.pair_table[idx]
-#             wav1, sr = torchaudio.load(x1_path)
-#             wav2, _ = torchaudio.load(x2_path)
-
-#             wav1 = wav1.squeeze(0)
-#             wav2 = wav2.squeeze(0)
-
-#             length1 = wav1.shape[0]
-#             length2 = wav2.shape[0]
-
-#             return wav1, wav2, torch.tensor(int(y_label[0])), torch.tensor(length1), torch.tensor(length2)
-#         else:
-#             path = random.sample(find_files(self.speakers[idx],ext=["wav"]), self.utterance_sample)
-#             x_list = []
-#             length_list = []
-#             index = []
-
-#             for i in range(len(path)):
-#                 wav, sr=torchaudio.load(path[i])
-#                 wav = wav.squeeze(0)
-#                 length = wav.shape[0]
-#                 if length > self.max_timestep:
-#                     x = wav[:self.max_timestep]
-#                     length = self.max_timestep
-#                 else:
-#                     x = wav
-#                 x_list.append(x)
-#                 index.append(torch.tensor(idx).long())
-#                 length_list.append(torch.tensor(length).long())
-            
-#             return x_list, torch.stack(length_list), torch.stack(index)
-        
-#     def collate_test_fn(self, data_sample):
-#         # wavs: [(wavs[0:utterance_length],lengths[0:utterance_length]), ...]
-#         wavs1 = []
-#         wavs2 = []
-#         ylabels = []
-#         lengths1 = []
-#         lengths2 = []
-
-#         for samples in data_sample:
-#             wavs1.append(samples[0])
-#             wavs2.append(samples[1])
-#             ylabels.append(samples[2])
-#             lengths1.append(samples[3])
-#             lengths2.append(samples[4])
-
-#         all_wav = []
-#         all_wav.extend(wavs1)
-#         all_wav.extend(wavs2)
-
-#         all_length = []
-#         all_length.extend(lengths1)
-#         all_length.extend(lengths2)
-
-#         # length_tensor = torch.stack(all_length)
-#         # back_wavs = pad_sequence(all_wav, batch_first=True)
-#         # back_length = length_tensor
-
-#         return all_wavs, all_length, ylabel
-
-
-#     def collate_train_fn(self,data_sample):
-#         # wavs: [(wavs[0:utterance_length],lengths[0:utterance_length]), ...]
-#         wavs = []
-#         lengths = []
-#         indexes = []
-
-#         for samples in data_sample:
-#             wavs.extend(samples[0])
-#             lengths.extend(samples[1])
-
-#         # length_tensor = torch.stack(lengths)
-#         # back_wavs = pad_sequence(wavs, batch_first=True)
-#         # back_length = length_tensor
-
-#         return wavs, lengths
-
-
 # Voxceleb 1 + 2 
 # preprocessing need seperate folder to dev, train, test
 class SpeakerVerifi_train(Dataset):
-    def __init__(self, directory_path,utter_sample=5):
+    def __init__(self, directory_path,max_timestep=16000*5,utter_sample=5):
 
         self.root = directory_path
         self.all_speakers = [f.path for f in os.scandir(self.root) if f.is_dir()]
         self.utter_number = utter_sample
         self.necessary_dict = self.processing()
         self.dataset = self.necessary_dict['spk_paths'] 
+        self.max_timestep = max_timestep
         
 
     def processing(self):
         
         speaker_num = len(self.all_speakers)
-        return {"spk_paths":self.all_speaker,"total_spk_num":speaker_num,"pair_table":None}
+        return {"spk_paths":self.all_speakers,"total_spk_num":speaker_num,"pair_table":None}
     
     def __len__(self):
         return self.necessary_dict['total_spk_num']
@@ -195,6 +67,11 @@ class SpeakerVerifi_train(Dataset):
             wav = wav.squeeze(0)
             length = wav.shape[0]
 
+            if length > self.max_timestep:
+                length = self.max_timestep
+                start = random.randint(0,length - self.max_timestep)
+                wav = wav[start:start+self.max_timestep]
+
             x_list.append(wav)
             length_list.append(torch.tensor(length).long())
 
@@ -210,16 +87,17 @@ class SpeakerVerifi_train(Dataset):
             wavs.extend(samples[0])
             lengths.extend(samples[1])
 
-        return wavs, lengths
+        return wavs, lengths, -1
 
 
-class SpeakerVerifi_test(Dataset):
-    def __init__(self, directory_path,meta_data=None):
+class SpeakerVerifi_dev(Dataset):
+    def __init__(self, directory_path, max_timestep, meta_data=None):
 
         self.root = directory_path
         self.meta_data = meta_data
         self.necessary_dict = self.processing()
-        self.dataset = self.necessary_dict['pair_table'] if self.necessary_dict['pair_table'] else self.necessary_dict['spk_paths']
+        self.max_timestep = max_timestep
+        self.dataset = self.necessary_dict['pair_table'] 
         
     def processing(self):
         pair_table = []
@@ -236,7 +114,82 @@ class SpeakerVerifi_test(Dataset):
         return len(self.necessary_dict['pair_table'])
 
     def __getitem__(self, idx):
-        y_label, x1_path, x2_path = self.pair_table[idx]
+        y_label, x1_path, x2_path = self.dataset[idx]
+        wav1, _ = torchaudio.load(x1_path)
+        wav2, _ = torchaudio.load(x2_path)
+
+        wav1 = wav1.squeeze(0)
+        wav2 = wav2.squeeze(0)
+
+        length1 = wav1.shape[0]
+
+        if length1 > self.max_timestep:
+            length1 = self.max_timestep
+            start = random.randint(0,length1 - self.max_timestep)
+            wav1 = wav1[start:start+self.max_timestep]
+
+        length2 = wav2.shape[0]
+
+        if length2 > self.max_timestep:
+            length2 = self.max_timestep
+            start = random.randint(0,length2 - self.max_timestep)
+            wav2 = wav1[start:start+self.max_timestep]
+
+
+        return wav1, wav2, \
+        torch.tensor(length1), torch.tensor(length2), \
+        torch.tensor(int(y_label[0])),
+    
+    def collate_fn(self, data_sample):
+        wavs1 = []
+        wavs2 = []
+        lengths1 = []
+        lengths2 = []
+        ylabels = []
+
+        for samples in data_sample:
+            wavs1.append(samples[0])
+            wavs2.append(samples[1])
+            lengths1.append(samples[2])
+            lengths2.append(samples[3])
+            ylabels.append(samples[4])
+
+        all_wavs = []
+        all_wavs.extend(wavs1)
+        all_wavs.extend(wavs2)
+
+        all_lengths = []
+        all_lengths.extend(lengths1)
+        all_lengths.extend(lengths2)
+
+        return all_wavs, all_lengths, ylabels
+
+
+
+class SpeakerVerifi_test(Dataset):
+    def __init__(self, directory_path,meta_data=None):
+
+        self.root = directory_path
+        self.meta_data = meta_data
+        self.necessary_dict = self.processing()
+        self.dataset = self.necessary_dict['pair_table'] 
+        
+    def processing(self):
+        pair_table = []
+        usage_list = open(self.meta_data, "r").readlines()
+        for pair in usage_list:
+            list_pair = pair.split()
+            pair_1= os.path.join(self.root, list_pair[1])
+            pair_2= os.path.join(self.root, list_pair[2])
+            one_pair = [list_pair[0],pair_1,pair_2 ]
+            pair_table.append(one_pair)
+        return {"spk_paths":None,"total_spk_num":None,"pair_table":pair_table}
+
+    def __len__(self):
+        return len(self.necessary_dict['pair_table'])
+
+    def __getitem__(self, idx):
+        y_label, x1_path, x2_path = self.dataset[idx]
         wav1, _ = torchaudio.load(x1_path)
         wav2, _ = torchaudio.load(x2_path)
 
--- benchmark/downstream/speaker_verifi/expert.py
+++ benchmark/downstream/speaker_verifi/expert.py
@@ -17,10 +17,17 @@ import random
 #-------------#
 import torch
 import torch.nn as nn
+import numpy as np
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 #-------------#
-from benchmark.downstream.phone_linear.model import Model
-from benchmark.downstream.phone_linear.dataset import PhoneDataset
+from benchmark.downstream.speaker_verifi.model import Model
+from benchmark.downstream.speaker_verifi.dataset import SpeakerVerifi_train, SpeakerVerifi_test, SpeakerVerifi_dev
+from benchmark.downstream.speaker_verifi.model import GE2E
+from benchmark.downstream.speaker_verifi.utils import EER, compute_metrics
+
+import IPython
+import pdb
 
 
 class DownstreamExpert(nn.Module):
@@ -37,32 +44,36 @@ class DownstreamExpert(nn.Module):
         and wav1 is in torch.FloatTensor
     """
 
-    def __init__(self, upstream_dim, downstream='phone_linear', datarc={}, modelrc={}, **kwargs):
+    def __init__(self, upstream_dim, downstream='speaker_classifi', datarc={}, modelrc={}, **kwargs):
         super(DownstreamExpert, self).__init__()
         self.upstream_dim = upstream_dim
         self.downstream = downstream
         self.datarc = datarc
         self.modelrc = modelrc
 
-        self.train_dataset = PhoneDataset('train', self.datarc['train_batch_size'], **datarc)
-        self.dev_dataset = PhoneDataset('dev', self.datarc['eval_batch_size'], **datarc)
-        self.test_dataset = PhoneDataset('test', self.datarc['eval_batch_size'], **datarc)
+        self.train_dataset = SpeakerVerifi_train(datarc['train']['file_path'], datarc['train']['max_timestep'],datarc['train']['utter_number'])
+        self.dev_dataset = SpeakerVerifi_dev(datarc['dev']['file_path'], datarc['dev']['max_timestep'], datarc['dev']['meta_data'])
+        self.test_dataset = SpeakerVerifi_test(datarc['test']['file_path'], datarc['test']['meta_data'])
+        
+        self.connector = nn.Linear(upstream_dim, modelrc['input_dim'])
 
-        self.model = Model(input_dim=self.upstream_dim, output_class_num=self.train_dataset.class_num)
-        self.objective = nn.CrossEntropyLoss()
+        self.model = Model(input_dim=modelrc['input_dim'], agg_module=self.modelrc['agg_module'])
+        self.objective = GE2E()
+        self.score_fn  = nn.CosineSimilarity(dim=-1)
+        self.eval_metric = EER
 
     def _get_train_dataloader(self, dataset):
         return DataLoader(
-            dataset, batch_size=1, # for bucketing
+            dataset, batch_size=self.datarc['train_batch_size'], 
             shuffle=True, num_workers=self.datarc['num_workers'],
-            drop_last=False, pin_memory=True
+            collate_fn=dataset.collate_fn
         )
 
     def _get_eval_dataloader(self, dataset):
         return DataLoader(
-            dataset, batch_size=1, # for bucketing
+            dataset, batch_size=self.datarc['eval_batch_size'],
             shuffle=False, num_workers=self.datarc['num_workers'],
-            drop_last=False, pin_memory=True
+            collate_fn=dataset.collate_fn
         )
 
     # Interface
@@ -77,25 +88,9 @@ class DownstreamExpert(nn.Module):
     def get_test_dataloader(self):
         return self._get_eval_dataloader(self.test_dataset)
 
-    def _match_length(self, inputs, labels):
-        """
-        Since the upstream extraction process can sometimes cause a mismatch
-        between the seq lenth of inputs and labels:
-        - if len(inputs) > len(labels), we truncate the final few timestamp of inputs to match the length of labels
-        - if len(inputs) < len(labels), we duplicate the last timestep of inputs to match the length of labels
-        Note that the length of labels should never be changed.
-        """
-        input_len, label_len = inputs.size(1), labels.size(-1)
-        if input_len > label_len:
-            inputs = inputs[:, :label_len, :]
-        elif input_len < label_len:
-            pad_vec = inputs[:, -1, :].unsqueeze(1) # (batch_size, 1, feature_dim)
-            inputs = torch.cat((inputs, pad_vec.repeat(1, label_len-input_len, 1)), dim=1) # (batch_size, seq_len, feature_dim), where seq_len == labels.size(-1)
-        return inputs, labels
-
     # Interface
-    def forward(self, features, labels,
-                records=None, logger=None, global_step=0):
+    def forward(self, features, lengths, labels,
+                records=None, logger=None, prefix=None, global_step=0):
         """
         Args:
             features:
@@ -121,20 +116,77 @@ class DownstreamExpert(nn.Module):
             loss:
                 the loss to be optimized, should not be detached
         """
-        if labels.size(0) == 1: labels = labels.squeeze(0) # hack bucketing
-        labels = torch.LongTensor(labels).to(features.device)
-
+        features_pad = pad_sequence(features, batch_first=True)
         
-        predicted = self.model(features)
-        predicted, labels = self._match_length(predicted, labels) # match the predicted instead of features
+        attention_mask = [torch.ones((feature.shape[0])) for feature in features] 
 
-        # cause logits are in (batch, seq, class) and labels are in (batch, seq)
-        # nn.CrossEntropyLoss expect to have (N, class) and (N,) as input
-        # here we flatten logits and labels in order to apply nn.CrossEntropyLoss
-        class_num = predicted.size(-1)
-        loss = self.objective(predicted.reshape(-1, class_num), labels.reshape(-1))
+        attention_mask_pad = pad_sequence(attention_mask,batch_first=True)
 
-        predicted_classid = predicted.max(dim=-1).indices
-        records['acc'] += (predicted_classid == labels).view(-1).cpu().float().tolist()
+        attention_mask_pad = (1.0 - attention_mask_pad) * -100000.0
 
-        return loss
+        features_pad = self.connector(features_pad)
+        agg_vec = self.model(features_pad, attention_mask_pad.cuda())
+
+        # normalize to unit vector 
+        agg_vec = agg_vec / torch.norm(agg_vec, dim=-1).unsqueeze(-1)
+
+        if self.training:
+            
+            GE2E_matrix = agg_vec.reshape(-1, self.train_dataset.utter_number, agg_vec.shape[-1])
+            loss = self.objective(GE2E_matrix)
+            
+            return loss
+        
+        else:
+            vec1, vec2 = self.separate_data(agg_vec, labels)
+            scores = self.score_fn(vec1,vec2).squeeze().cpu().detach().tolist()
+            ylabels = torch.stack(labels).cpu().detach().long().tolist()
+
+            if len(ylabels) > 1:
+                records['scores'].extends(scores)
+                records['ylabels'].extends(ylabels)
+            else:
+                records['scores'].append(scores)
+                records['ylabels'].append(ylabels)
+
+            return torch.tensor(0)
+
+        # interface
+    def log_records(self, records, logger, prefix, global_step):
+        """
+        Args:
+            records:
+                defaultdict(list), contents already appended
+
+            logger:
+                Tensorboard SummaryWriter
+                please use f'{prefix}your_content_name' as key name
+                to log your customized contents
+
+            prefix:
+                used to indicate downstream and train/test on Tensorboard
+                eg. 'phone/train-'
+
+            global_step:
+                global_step in runner, which is helpful for Tensorboard logging
+        """
+
+        if not self.training:
+
+            EER_result =self.eval_metric(np.array(records['ylabels']), np.array(records['scores']))
+
+            records['EER'] = EER_result[0]
+
+            logger.add_scalar(
+                f'{prefix}'+'EER',
+                records['EER'],
+                global_step=global_step
+            )
+        
+    def separate_data(self, agg_vec, ylabel):
+
+        total_num = len(ylabel) 
+        feature1 = agg_vec[:total_num]
+        feature2 = agg_vec[total_num:]
+        
+        return feature1, feature2
