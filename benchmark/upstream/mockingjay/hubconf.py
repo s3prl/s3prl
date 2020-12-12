d5d67bbda010f545dc33f666ccbcfe3783d7ab88
Leo Yang <leo19941227@gmail.com>
2020-12-13 02:38:21 +0800

    automatically expose UpstreamExpert to hubconf

---
+++ benchmark/upstream/mockingjay/hubconf.py
@@ -0,0 +1,23 @@
+import torch
+
+from hubconf import _gdown
+from .expert import UpstreamExpert as _UpstreamExpert
+
+
+def mockingjay(ckpt=None, *args, **kwargs):
+    """
+    The Mockingjay model
+        ckpt (str): kwargs, path to the pretrained weights of the model.
+    """
+    upstream = _UpstreamExpert(ckpt)
+    return upstream
+
+
+def mockingjay_default(use_cache=True, *args, **kwargs):
+    """
+    The Mockingjay model from https://drive.google.com/u/1/uc?id=1MoF_poVUaL3tKe1tbrQuDIbsC38IMpnH
+        use_cache (bool): whether to download ckpt/config again if existed
+    """
+    url = 'https://drive.google.com/u/1/uc?id=1MoF_poVUaL3tKe1tbrQuDIbsC38IMpnH'
+    ckpt = _gdown('mockingjay_default.ckpt', url, use_cache)
+    return mockingjay(ckpt)
