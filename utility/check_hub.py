fcd8d75e91e2e440f473aa98a550d010750e55bb
Leo Yang <leo19941227@gmail.com>
2020-12-14 04:13:38 +0800

    add script to aid checking upstream usage

--- utility/check_hub.py
+++ utility/check_hub.py
@@ -8,8 +8,10 @@ parser = argparse.ArgumentParser()
 upstreams = [attr for attr in dir(hubconf) if callable(getattr(hubconf, attr)) and attr[0] != '_']
 parser.add_argument('--mode', choices=['list', 'help', 'load'], required=True)
 parser.add_argument('--upstream', choices=upstreams)
+parser.add_argument('--ckpt', help='The PATH/URL/GOOGLE_DRIVE_ID of upstream checkpoint, not always needed')
+parser.add_argument('--config', help='The PATH of upstream config, not always needed')
 parser.add_argument('--refresh', action='store_true', help='Whether to re-download upstream contents')
-parser.add_argument('--ckpt', help='The PATH/URL/GOOGLE_DRIVE_ID of upstream checkpoint')
+
 args = parser.parse_args()
 
 REPO = 'andi611/Self-Supervised-Speech-Pretraining-and-Representation-Learning:benchmark'
@@ -21,4 +23,7 @@ elif args.mode == 'help':
     print(torch.hub.help(REPO, args.upstream, force_reload=args.refresh))
 
 elif args.mode == 'load':
-    print(torch.hub.load(REPO, args.upstream, ckpt=args.ckpt, force_reload=args.refresh, refresh=args.refresh))
+    print(torch.hub.load(
+        REPO, args.upstream, force_reload=args.refresh,
+        ckpt=args.ckpt, config=args.config, refresh=args.refresh
+    ))
