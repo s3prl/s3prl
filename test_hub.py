34ea379e689b3ef0feb338a03885148ef4e0d2aa
Leo Yang <leo19941227@gmail.com>
2020-12-13 20:19:15 +0800

    upstea test_hub script

--- test_hub.py
+++ test_hub.py
@@ -6,9 +6,19 @@ import hubconf
 
 parser = argparse.ArgumentParser()
 upstreams = [attr for attr in dir(hubconf) if callable(getattr(hubconf, attr)) and attr[0] != '_']
-parser.add_argument('--upstream', choices=upstreams, required=True)
+parser.add_argument('--mode', choices=['list', 'help', 'load'], required=True)
+parser.add_argument('--upstream', choices=upstreams)
 parser.add_argument('--refresh', action='store_true', help='Whether to re-download upstream contents')
+parser.add_argument('--ckpt', help='The PATH/URL/GOOGLE_DRIVE_ID of upstream checkpoint')
 args = parser.parse_args()
 
 REPO = 'andi611/Self-Supervised-Speech-Pretraining-and-Representation-Learning:benchmark'
-model = torch.hub.load(REPO, args.upstream, force_reload=args.refresh, refresh=args.refresh)
+
+if args.mode == 'list':
+    print(torch.hub.list(REPO, force_reload=args.refresh))
+
+elif args.mode == 'help':
+    print(torch.hub.help(REPO, args.upstream, force_reload=args.refresh))
+
+elif args.mode == 'load':
+    print(torch.hub.load(REPO, args.upstream, ckpt=args.ckpt, force_reload=args.refresh, refresh=args.refresh))
