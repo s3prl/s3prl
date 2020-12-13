7b0917cfca771bc4a527034cc9d4ced49d6cba27
Leo Yang <leo19941227@gmail.com>
2020-12-13 20:23:07 +0800

    tera support url/google id

--- benchmark/upstream/tera/hubconf.py
+++ benchmark/upstream/tera/hubconf.py
@@ -1,25 +1,44 @@
 import os
 import torch
+from functools import partial as _partial
 
-from hubconf import _gdown
+from hubconf import _url_wrapper, _gdriveid_wrapper
 from .expert import UpstreamExpert as _UpstreamExpert
+
+MODEL = 'TERA'
 
 
 def tera(ckpt, *args, **kwargs):
-    """
-    The TERA model
-        ckpt (str): kwargs, path to the pretrained weights of the model.
+    f"""
+        The {MODEL} model from local ckpt
+            ckpt (str): PATH
     """
     assert os.path.isfile(ckpt)
     upstream = _UpstreamExpert(ckpt)
     return upstream
 
 
-def tera_default(refresh=False, *args, **kwargs):
-    """
-    The TERA model from https://drive.google.com/u/1/uc?id=1A9Fs2k3aekY4_6I2GD4tBtjx_v0mV_k4
+tera_url = _partial(_url_wrapper, cls=tera)
+tera_url.__doc__ =\
+f"""
+    The {MODEL} model from url
+        ckpt (str): URL
         refresh (bool): whether to download ckpt/config again if existed
+"""
+
+
+tera_gdriveid = _partial(_gdriveid_wrapper, cls=tera)
+tera_gdriveid.__doc__ =\
+f"""
+    The {MODEL} model from google drive id
+        ckpt (str): The unique id in the google drive share link
+        refresh (bool): whether to download ckpt/config again if existed
+"""
+
+
+def tera_default(refresh=False, *args, **kwargs):
+    f"""
+        The default {MODEL} model
+            refresh (bool): whether to download ckpt/config again if existed
     """
-    url = 'https://drive.google.com/u/1/uc?id=1A9Fs2k3aekY4_6I2GD4tBtjx_v0mV_k4'
-    ckpt = _gdown('tera_default.ckpt', url, refresh)
-    return tera(ckpt)
+    return tera_gdriveid(ckpt='1A9Fs2k3aekY4_6I2GD4tBtjx_v0mV_k4', refresh=refresh)
