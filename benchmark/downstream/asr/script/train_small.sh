196e20aecfbc117896142e6c9f40580e5ebc3ca8
Leo Yang <leo19941227@gmail.com>
2020-12-14 17:06:41 +0800

    add small model config for debugging

---
+++ benchmark/downstream/asr/script/train_small.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $1 : experiment name
+# $2 : cuda id
+
+CONFIG="small"
+
+DIR="/home/leo/d/Benchmarking/S3PRL/benchmark/downstream/asr"
+
+echo "Start running training process of E2E ASR"
+CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
+    --name $1 \
+    --njobs 16
+    --seed 0 \
+    --logdir ${DIR}/log/ \
+    --ckpdir ${DIR}/ckpt/ \
+    --outdir ${DIR}/result/ \
+    --reserve_gpu 0 \
+
