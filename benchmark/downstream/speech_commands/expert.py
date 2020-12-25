862f8c8fb0ab5a05626c2b16d83f99872d5a0a42
Yist Lin <yishen992@gmail.com>
2020-12-25 16:48:57 +0800

    refactor speech commands expert.py

--- benchmark/downstream/speech_commands/expert.py
+++ benchmark/downstream/speech_commands/expert.py
@@ -3,7 +3,7 @@
 import re
 import hashlib
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -75,50 +75,6 @@ class DownstreamExpert(nn.Module):
 
     # Interface
     def forward(self, features, labels, records, logger, prefix, global_step, **kwargs):
-        """
-        This function will be used in both train/dev/test, you can use
-        self.training (bool) to control the different behavior for
-        training or evaluation (dev/test)
-
-        Args:
-            features:
-                list of unpadded features [feat1, feat2, ...]
-                each feat is in torch.FloatTensor and already
-                put in the device assigned by command-line args
-
-            your_other_contents1, ... :
-                in the order defined by your dataloader (dataset + collate_fn)
-                these are all in cpu, and you can move them to the same device
-                as features
-
-            records:
-                defaultdict(list), by dumping contents into records,
-                these contents can be averaged and logged on Tensorboard
-                later by self.log_records
-
-                Note1. benchmark/runner.py will call self.log_records
-                    1. every log_step during training
-                    2. once after evalute the whole dev/test dataloader
-
-                Note2. log_step is defined in your downstream config
-
-            logger:
-                Tensorboard SummaryWriter, given here for logging/debugging convenience
-                please use f'{prefix}your_content_name' as key name
-                to log your customized contents
-
-            prefix:
-                used to indicate downstream and train/test on Tensorboard
-                eg. 'phone/train-'
-
-            global_step:
-                global_step in runner, which is helpful for Tensorboard logging
-
-        Return:
-            loss:
-                the loss to be optimized, should not be detached
-                a single scalar in torch.FloatTensor
-        """
         features = pad_sequence(features, batch_first=True)
         predicted = self.model(features)
 
@@ -132,63 +88,42 @@ class DownstreamExpert(nn.Module):
 
     # interface
     def log_records(self, records, logger, prefix, global_step, **kwargs):
-        """
-        This function will be used in both train/dev/test, you can use
-        self.training (bool) to control the different behavior for
-        training or evaluation (dev/test)
-
-        Args:
-            records:
-                defaultdict(list), contents already prepared by self.forward
-
-            logger:
-                Tensorboard SummaryWriter
-                please use f'{prefix}your_content_name' as key name
-                to log your customized contents
-
-            prefix:
-                used to indicate downstream and train/test on Tensorboard
-                eg. 'phone/train-'
-
-            global_step:
-                global_step in runner, which is helpful for Tensorboard logging
-        """
         for key, values in records.items():
             average = torch.FloatTensor(values).mean().item()
             logger.add_scalar(f"{prefix}{key}", average, global_step=global_step)
 
 
 def split_dataset(
-    speech_commands_root, max_uttr_per_class=2 ** 27 - 1
+    root_dir: Union[str, Path], max_uttr_per_class=2 ** 27 - 1
 ) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
-    """Split all speech commands into 3 set.
+    """Split Speech Commands into 3 set.
+    
+    Args:
+        root_dir: speech commands dataset root dir
+        max_uttr_per_class: predefined value in the original paper
     
     Return:
-        train_list: [(class, audio_path), ...]
+        train_list: [(class_name, audio_path), ...]
         valid_list: as above
     """
-    data_root_path = Path(speech_commands_root)
-
-    all_classes = [
-        entry.name
-        for entry in data_root_path.iterdir()
-        if entry.is_dir() and entry.name != "_background_noise_"
-    ]
-
     train_list, valid_list = [], []
 
-    for class_name in all_classes:
-        for audio_path in (data_root_path / class_name).glob("*.wav"):
+    for entry in Path(root_dir).iterdir():
+        if not entry.is_dir() or entry.name == "_background_noise_":
+            continue
+
+        for audio_path in entry.glob("*.wav"):
             speaker_hashed = re.sub(r"_nohash_.*$", "", audio_path.name)
             hashed_again = hashlib.sha1(speaker_hashed.encode("utf-8")).hexdigest()
             percentage_hash = (int(hashed_again, 16) % (max_uttr_per_class + 1)) * (
                 100.0 / max_uttr_per_class
             )
+
             if percentage_hash < 10:
-                valid_list.append((class_name, audio_path))
+                valid_list.append((entry.name, audio_path))
             elif percentage_hash < 20:
                 pass  # testing set is discarded
             else:
-                train_list.append((class_name, audio_path))
+                train_list.append((entry.name, audio_path))
 
     return train_list, valid_list
