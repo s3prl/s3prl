f09e5ad455a141009cc6c1646d9f7920705b0a38
andi611 <tingweiandyliu@gmail.com>
2020-12-31 12:58:37 +0800

    update pretrain deterministic

--- run_pretrain.py
+++ run_pretrain.py
@@ -115,9 +115,11 @@ def main():
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    if torch.cuda.is_available(): torch.cuda.manual_seed_all(args.seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+        torch.set_deterministic(True)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
     runner = Runner(args, config)
     eval('runner.train')()
