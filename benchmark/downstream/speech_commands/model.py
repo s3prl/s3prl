84592d57194074aa016587ea83ca1f71c42570d2
Leo Yang <leo19941227@gmail.com>
2021-01-17 12:26:20 +0800

    unify probing protocol

--- benchmark/downstream/speech_commands/model.py
+++ benchmark/downstream/speech_commands/model.py
@@ -5,10 +5,14 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self, input_dim, output_class_num, **kwargs):
         super(Model, self).__init__()
-        self.fc1 = nn.Linear(input_dim, kwargs["hidden_dim"])
-        self.fc2 = nn.Linear(kwargs["hidden_dim"], output_class_num)
+        hidden_dim = kwargs["hidden_dim"]
+        self.connector = nn.Linear(input_dim, hidden_dim)
+        self.fc1 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_class_num)
 
     def forward(self, features):
+        features = F.relu(self.connector(features))
+        features = self.fc1(features)
         pooled = features.mean(dim=1)
-        predicted = self.fc2(F.relu(self.fc1(pooled)))
+        predicted = self.fc2(pooled)
         return predicted
