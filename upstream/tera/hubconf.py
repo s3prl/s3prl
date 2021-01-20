04b808aa017221d4fd6fe39ddf3a7c046a86f9c4
andi611 <tingweiandyliu@gmail.com>
2021-01-20 13:40:09 +0800

    update tera ckpt

--- upstream/tera/hubconf.py
+++ upstream/tera/hubconf.py
@@ -36,7 +36,7 @@ def tera(refresh=False, *args, **kwargs):
         The default model
             refresh (bool): whether to download ckpt/config again if existed
     """
-    return tera_logMelBase_time_freq_mag_AdamW_b32_1m_960hr(refresh, *args, **kwargs)
+    return tera_logMelBase_time_freq_AdamW_b32_1m_960hr(refresh, *args, **kwargs)
 
 
 ##########
@@ -53,8 +53,8 @@ def tera_logMelBase_time_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1-JwGlb3JXXnnXqKL9WVtbv34RR0o6BAX'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/luorglf8mdg67l2/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_freq_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -66,8 +66,8 @@ def tera_logMelBase_freq_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1ZqTQx6vCqSPFbqtDwL-LjKKpoKrga3cz'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/ol873nw97gyjw3l/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -79,8 +79,8 @@ def tera_logMelBase_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1EE5ieH-bsNynD3COYI8_yS8xxSnL4zFp'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/gg9ft4jwtxadh0a/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_freq_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -92,8 +92,8 @@ def tera_logMelBase_time_freq_AdamW_b32_200k_100hr(refresh=False, *args, **kwarg
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1g2RBcl4xxvpDtA7eeSIGgygNrMGMUxu2'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/o36qt1zgtn3tsep/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -105,8 +105,8 @@ def tera_logMelBase_time_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1eQrzJ138OXqMTQfNEl_rsT1Y_GTMj0mQ'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/w8g0nqsqpfldij5/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_freq_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -118,8 +118,8 @@ def tera_logMelBase_freq_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '1IagYPw9IPMS_8-jwBmduorzjuRyxrsyY'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/jztwgjsh1o74wj2/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_freq_mag_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
@@ -131,8 +131,8 @@ def tera_logMelBase_time_freq_mag_AdamW_b32_200k_100hr(refresh=False, *args, **k
         Total steps: 200k
         Unlabled Speech: 100hr
     """
-    kwargs['ckpt'] = '12gUQ6RHyujPp7ZoSsr3t1xrsySsoxVOd'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/l9ryl82k64m1lsk/states-200000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 ##########
@@ -149,21 +149,8 @@ def tera_logMelBase_time_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = 'http://140.112.21.12:8000/tera/logMelBase_time_AdamW_b32_1m_960hr/states-1000000.ckpt'
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/jzx0xggk663jev6/states-1000000.ckpt?dl=0'
     return tera_url(refresh=refresh, *args, **kwargs)
-
-
-def tera_logMelBase_time_AdamW_b32_500k_960hr(refresh=False, *args, **kwargs):
-    """
-        Feature: 80-dim log Mel
-        Alteration: time
-        Optimizer: AdamW
-        Batch size: 32
-        Total steps: 500k
-        Unlabled Speech: 960hr
-    """
-    kwargs['ckpt'] = '1ZGDeQUoiUapRGuhvUBmJaE-XTyDceCHf'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_freq_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -175,8 +162,8 @@ def tera_logMelBase_freq_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = '1MezF4LHnu-E8ZQF_pN6Jy7rEp8R0PmQu'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/vj047t6257gi1al/states-1000000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -188,8 +175,8 @@ def tera_logMelBase_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = '1DPI3G34_TI0_DwkckDteH5QKx43y1R7u'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/k3cye2cxcsuplrw/states-1000000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_freq_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -201,8 +188,8 @@ def tera_logMelBase_time_freq_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs)
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = '1ZmsLfRKilrSn11U3vwPl0DVHIul3xmpz'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/98olxex0m7oy9ta/states-1000000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -214,8 +201,8 @@ def tera_logMelBase_time_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = '1Uwlfcmwab8EL-dniBaI7AkVyAEHsLXnm'
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/i3p2ulhd7robwcl/states-1000000.ckpt?dl=0'
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_freq_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -228,7 +215,7 @@ def tera_logMelBase_freq_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
         Unlabled Speech: 960hr
     """
     kwargs['ckpt'] = ''
-    return tera_gdriveid(refresh=refresh, *args, **kwargs)
+    return tera_url(refresh=refresh, *args, **kwargs)
 
 
 def tera_logMelBase_time_freq_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwargs):
@@ -240,5 +227,23 @@ def tera_logMelBase_time_freq_mag_AdamW_b32_1m_960hr(refresh=False, *args, **kwa
         Total steps: 1M
         Unlabled Speech: 960hr
     """
-    kwargs['ckpt'] = 'http://140.112.21.12:8000/tera/logMelBase_time_freq_mag_AdamW_b32_1m_960hr/states-1000000.ckpt'
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/vbagkhl2tgh7fff/states-1000000.ckpt?dl=0'
     return tera_url(refresh=refresh, *args, **kwargs)
+
+
+#####################
+# Other Feat 100 HR #
+#####################
+
+
+def tera_fbankBase_time_freq_AdamW_b32_200k_100hr(refresh=False, *args, **kwargs):
+    """
+        Feature: 240-dim fbank
+        Alteration: time + freq
+        Optimizer: AdamW
+        Batch size: 32
+        Total steps: 200k
+        Unlabled Speech: 100hr
+    """
+    kwargs['ckpt'] = 'https://www.dropbox.com/s/i32ob29m6afufot/states-200000.ckpt?dl=0'
+    return tera_gdriveid(refresh=refresh, *args, **kwargs)
