6001b512d1dbcd0ff41759b2cdedeedccf021b0d
andi611 <tingweiandyliu@gmail.com>
2021-01-26 01:40:33 +0800

    update bucketing preprocessing

--- preprocess/generate_len_for_bucket.py
+++ preprocess/generate_len_for_bucket.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- #
 """*********************************************************************************************"""
-#   FileName     [ preprocess_any.py ]
-#   Synopsis     [ preprocess text transcripts and audio speech for any dataset ]
+#   FileName     [ generate_len_for_bucket.py ]
+#   Synopsis     [ preprocess audio speech to generate meta data for dataloader bucketing ]
 #   Author       [ Andy T. Liu (Andi611) ]
 #   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
 #   Reference    [ https://github.com/Alexander-H-Liu/End-to-end-ASR-Pytorch ]
@@ -23,7 +23,9 @@ from pathlib import Path
 from joblib import Parallel, delayed
 
 
-# SETS = ['train', 'dev', 'test']  # change these to match your dataset
+# change these to match your dataset
+# SETS = ['train', 'dev', 'test']
+# SETS = ['TRAIN', 'TEST']
 SETS = ['train-clean-100', 'train-clean-360', 'train-other-500', 'dev-clean', 'dev-other', 'test-clean', 'test-other']
 
 
@@ -68,9 +70,8 @@ def generate_length(args, tr_set, audio_extention):
     
     for i, s in enumerate(tr_set):
         print('')
-        print('Preprocessing data in: ', s, end='')
         todo = list(Path(os.path.join(args.data_root,s)).rglob('*' + audio_extention)) # '*.flac'
-        print(len(todo), 'audio files found.')
+        print(f'Preprocessing data in: {s}, {len(todo)} audio files found.')
 
         output_dir = os.path.join(args.output_path, args.name)
         if not os.path.exists(output_dir): os.makedirs(output_dir)
@@ -79,7 +80,7 @@ def generate_length(args, tr_set, audio_extention):
         tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_length)(str(file)) for file in tqdm(todo))
 
         # sort by len
-        sorted_todo = [os.path.join(tr_set[i], str(todo[idx]).split('/')[-1].replace(audio_extention, '.npy')) for idx in reversed(np.argsort(tr_x))]
+        sorted_todo = [os.path.join(s, str(todo[idx]).split(s+'/')[-1]) for idx in reversed(np.argsort(tr_x))]
         # Dump data
         df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo], 'length':list(reversed(sorted(tr_x))), 'label':None})
         df.to_csv(os.path.join(output_dir, tr_set[i] + '.csv'))
@@ -94,6 +95,11 @@ def main():
 
     # get arguments
     args = get_preprocess_args()
+    
+    if 'librispeech' in args.data_root.lower():
+        SETS = ['train-clean-100', 'train-clean-360', 'train-other-500', 'dev-clean', 'dev-other', 'test-clean', 'test-other']
+    elif 'timit' in args.data_root.lower():
+        SETS = ['TRAIN', 'TEST']
 
     # Select data sets
     for idx, s in enumerate(SETS):
