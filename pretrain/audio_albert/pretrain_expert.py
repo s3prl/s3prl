2d318658cd22e5886e96c5efada0109ad775b125
andi611 <tingweiandyliu@gmail.com>
2021-01-27 21:35:08 +0800

    add audio albert pretraining

---
+++ pretrain/audio_albert/pretrain_expert.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ pretrain_expert.py ]
+#   Synopsis     [ the audio albert pretrain expert ]
+#   Author       [ S3PRL ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+from pretrain.mockingjay.pretrain_expert import UpstreamPretrainExpert
