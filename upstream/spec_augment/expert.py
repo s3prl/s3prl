71364fab0a73c6884e3515a6f8ad99b95512609f
andi611 <tingweiandyliu@gmail.com>
2021-02-03 20:05:52 +0800

    add spec_augment upstream

---
+++ upstream/spec_augment/expert.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ expert.py ]
+#   Synopsis     [ the spec augment wrapper ]
+#   Author       [ S3PRL ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+from ..mockingjay.expert import UpstreamExpert
