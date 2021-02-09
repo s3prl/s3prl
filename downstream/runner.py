d39828857b7062af79146839a5b3bf289dfe47d6
Leo Yang <leo19941227@gmail.com>
2021-02-09 23:09:44 +0800

    support decoar 1.0 (mxnet, can't tune for now)

--- downstream/runner.py
+++ downstream/runner.py
@@ -14,7 +14,7 @@ from tensorboardX import SummaryWriter
 
 from optimizers import get_optimizer
 from schedulers import get_scheduler
-from utility.helper import count_parameters, count_used_parameters
+from utility.helper import count_parameters
 
 SAMPLE_RATE = 16000
 
@@ -52,13 +52,10 @@ class Runner():
         assert hasattr(upstream, 'get_output_dim')
         assert hasattr(upstream, 'get_downsample_rate')
 
-        upstream([torch.randn(16000, requires_grad=True).to(self.args.device)])[0].sum().backward()
         print(f'[Runner] - Upstream model architecture: {upstream}')
-        print(f'[Runner] - Upstream has {count_used_parameters(upstream)} parameters')
         print(f'[Runner] - Upstream output dimension: {upstream.get_output_dim()}')
         downsample = upstream.get_downsample_rate()
         print(f'[Runner] - Upstream downsample rate: {downsample} ({downsample / SAMPLE_RATE * 1000} ms/frame)')
-        upstream.zero_grad()
 
         init_upstream = self.init_ckpt.get('Upstream')
         if init_upstream:
