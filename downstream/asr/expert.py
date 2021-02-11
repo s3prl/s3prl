6729403dfe0558503cc2dac2385a3dcd2bccaa0d
simpleoier <netnetchangxk@gmail.com>
2021-02-11 07:02:46 +0000

    initial commit for ASR downstream task, add dataset, specaug support

---
+++ downstream/asr/expert.py
@@ -0,0 +1,203 @@
+import os
+import math
+import torch
+import random
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+
+from .model import Model
+from .dataset import SequenceDataset
+
+
+class DownstreamExpert(nn.Module):
+    """
+    Used to handle downstream-specific operations
+    eg. downstream forward, metric computation, contents to log
+    """
+
+    def __init__(self, upstream_dim, upstream_rate, downstream_expert, expdir, **kwargs):
+        """
+        Args:
+            upstream_dim: int
+                Different upstream will give different representation dimension
+                You might want to first project them to the same dimension
+
+            upstream_rate: int
+                160: for upstream with 10 ms per frame
+                320: for upstream with 20 ms per frame
+            
+            downstream_expert: dict
+                The 'downstream_expert' field specified in your downstream config file
+                eg. downstream/example/config.yaml
+
+            expdir: string
+                The expdir from command-line argument, you should save all results into
+                this directory, like some logging files.
+
+            **kwargs: dict
+                All the arguments specified by the argparser in run_downstream.py
+                and all the other fields in config.yaml, in case you need it.
+                
+                Note1. Feel free to add new argument for __init__ as long as it is
+                a command-line argument or a config field. You can check the constructor
+                code in downstream/runner.py
+        """
+
+        super(DownstreamExpert, self).__init__()
+        self.upstream_dim = upstream_dim
+        self.datarc = downstream_expert['datarc']
+        self.modelrc = downstream_expert['modelrc']
+
+        self.train_dataset = SequenceDataset("train", self.datarc['train_batch_size'], **self.datarc)
+        self.dev_dataset = SequenceDataset("dev", self.datarc['eval_batch_size'], **self.datarc)
+        self.test_dataset = SequenceDataset("test", self.datarc['eval_batch_size'], **self.datarc)
+
+        self.connector = nn.Linear(upstream_dim, self.modelrc['input_dim'])
+        self.model = Model(
+            output_class_num=self.train_dataset.class_num,
+            **self.modelrc
+        )
+        self.objective = nn.CrossEntropyLoss()
+
+
+    # Interface
+    def get_dataloader(self, mode):
+        """
+        Args:
+            mode: string
+                'train', 'dev' or 'test'
+
+        Return:
+            a torch.utils.data.DataLoader returning each batch in the format of:
+
+            [wav1, wav2, ...], your_other_contents1, your_other_contents2, ...
+
+            where wav1, wav2 ... are in variable length
+            each wav is torch.FloatTensor in cpu with:
+                1. dim() == 1
+                2. sample_rate == 16000
+                3. directly loaded by torchaudio
+        """
+
+        if mode == 'train':
+            return self._get_train_dataloader(self.train_dataset)            
+        elif mode == 'dev':
+            return self._get_eval_dataloader(self.dev_dataset)
+        elif mode == 'test':
+            return self._get_eval_dataloader(self.test_dataset)
+
+
+    def _get_train_dataloader(self, dataset):
+        return DataLoader(
+            dataset, batch_size=self.datarc['train_batch_size'],
+            shuffle=True, num_workers=self.datarc['num_workers'],
+            collate_fn=dataset.collate_fn
+        )
+
+
+    def _get_eval_dataloader(self, dataset):
+        return DataLoader(
+            dataset, batch_size=self.datarc['eval_batch_size'],
+            shuffle=False, num_workers=self.datarc['num_workers'],
+            collate_fn=dataset.collate_fn
+        )
+
+
+    # Interface
+    def forward(self, mode, features, your_other_contents1, records, **kwargs):
+        """
+        Args:
+            mode: string
+                'train', 'dev' or 'test' for this forward step
+
+            features:
+                list of unpadded features [feat1, feat2, ...]
+                each feat is in torch.FloatTensor and already
+                put in the device assigned by command-line args
+
+            your_other_contents1, ... :
+                in the order defined by your dataloader (dataset + collate_fn)
+                these are all in cpu, and you can move them to the same device
+                as features
+
+            records:
+                defaultdict(list), by appending contents into records,
+                these contents can be averaged and logged on Tensorboard
+                later by self.log_records (also customized by you)
+
+                Note1. downstream/runner.py will call self.log_records
+                    1. every `log_step` during training
+                    2. once after evalute the whole dev/test dataloader
+
+                Note2. `log_step` is defined in your downstream config
+                eg. downstream/example/config.yaml
+
+        Return:
+            loss:
+                the loss to be optimized, should not be detached
+                a single scalar in torch.FloatTensor
+        """
+        features = pad_sequence(features, batch_first=True)
+        features = self.connector(features)
+        predicted = self.model(features)
+
+        utterance_labels = your_other_contents1
+        labels = torch.LongTensor(utterance_labels).to(features.device)
+        loss = self.objective(predicted, labels)
+
+        predicted_classid = predicted.max(dim=-1).indices
+
+        records['loss'].append(loss.item())
+        records['acc'] += (predicted_classid == labels).view(-1).cpu().float().tolist()
+
+        return loss
+
+
+    # interface
+    def log_records(self, mode, records, logger, global_step, batch_ids, total_batch_num, **kwargs):
+        """
+        Args:
+            mode: string
+                'train':
+                    records and batchids contain contents for `log_step` batches
+                    `log_step` is defined in your downstream config
+                    eg. downstream/example/config.yaml
+
+                'dev' or 'test' :
+                    records and batchids contain contents for the entire evaluation dataset
+
+            records:
+                defaultdict(list), contents already prepared by self.forward
+
+            logger:
+                Tensorboard SummaryWriter
+                please use f'{your_task_name}/{mode}-{key}' as key name to log your contents,
+                preventing conflict with the logging of other tasks
+
+            global_step:
+                The global_step when training, which is helpful for Tensorboard logging
+
+            batch_ids:
+                The batches contained in records when enumerating over the dataloader
+
+            total_batch_num:
+                The total amount of batches in the dataloader
+        
+        Return:
+            a list of string
+                Each string is a filename we wish to use to save the current model
+                according to the evaluation result, like the best.ckpt on the dev set
+                You can return nothing or an empty list when no need to save the checkpoint
+        """
+        for key, values in records.items():
+            average = torch.FloatTensor(values).mean().item()
+            logger.add_scalar(
+                f'example/{mode}-{key}',
+                average,
+                global_step=global_step
+            )
+
+        return [f'best-{mode}.ckpt']
---
+++ downstream/asr/specaug.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*- #
+"""*********************************************************************************************"""
+#   FileName     [ dataset.py ]
+#   Synopsis     [ the phone dataset ]
+#   Author       [ S3PRL, Xuankai Chang ]
+#   Copyright    [ Copyleft(c), Speech Lab, NTU, Taiwan ]
+"""*********************************************************************************************"""
+
+
+###############
+# IMPORTATION #
+###############
+import os
+import random
+#-------------#
+import pandas as pd
+#-------------#
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data.dataset import Dataset
+#-------------#
+import torchaudio
+
+
+DEFAULT_TIME_WARP_MODE = "bicubic"
+
+class SpecAug(torch.nn.Module):
+    """SpecAug"""
+    def __init__(
+        self,
+        apply_time_warp=True,
+        time_warp_window=5,
+        time_warp_mode="bicubic",
+        apply_freq_mask=True,
+        freq_mask_width_range=(0,20),
+        num_freq_mask=2,
+        apply_time_mask=True,
+        time_mask_width_range=(0,100),
+        num_time_mask=2,
+    ):
+        assert any([apply_time_warp, apply_freq_mask, apply_time_mask])
+
+        super(SpecAug, self).__init__()
+        self.apply_time_warp = apply_time_warp
+        self.apply_freq_mask = apply_freq_mask
+        self.apply_time_mask = apply_time_mask
+
+        if apply_time_warp:
+            self.time_warp = TimeWarp(window=time_warp_window, mode=time_warp_mode)
+        else:
+            self.time_warp = None
+
+        if apply_freq_mask:
+            self.freq_mask = MaskAlongAxis(
+                dim="freq",
+                mask_width_range=freq_mask_width_range,
+                num_mask=num_freq_mask,
+            )
+        else:
+            self.freq_mask = None
+
+        if apply_time_mask:
+            self.time_mask = MaskAlongAxis(
+                dim="time",
+                mask_width_range=time_mask_width_range,
+                num_mask=num_time_mask,
+            )
+        else:
+            self.time_mask = None
+
+    def apply_specaug(self, x, x_lengths=None):
+        if self.time_warp is not None:
+            x, x_lengths = self.time_warp(x, x_lengths)
+        if self.freq_mask is not None:
+            x, x_lengths = self.freq_mask(x, x_lengths)
+        if self.time_mask is not None:
+            x, x_lengths = self.time_mask(x, x_lengths)
+        return x, x_lengths
+
+    def forward(self, xs, x_lengths=None):
+        """Forward SpecAug.
+        
+        Args:
+            xs: list of features [(T, D)] x batchsize
+            x_lengths: length of features
+        Return:
+            list of features[(T, D)] x batchsize
+        """
+        assert len(xs[0].size()) == 2
+        if x_lengths is None:
+            x_lengths = torch.LongTensor([x.size(0) for x in xs])
+
+        batchsize, max_len, dim = len(x_lengths), torch.max(x_lengths).item(), xs[0].size(1)
+        xs_pad = xs[0].new_zeros((batchsize, max_len, dim))
+        for i, x in enumerate(xs):
+            xs_pad[i, :x_lengths[i]] = x
+
+        xs_pad, _ = self.apply_specaug(xs_pad, x_lengths)
+
+        xs = [xs_pad[i, :xs[i].size(0)] for i in range(batchsize)]
+        return xs, x_lengths
+
+
+class TimeWarp(torch.nn.Module):
+    """Time warping using torch.interpolate.
+    Args:
+        window: time warp parameter
+        mode: Interpolate mode
+    """
+
+    def __init__(self, window=80, mode=DEFAULT_TIME_WARP_MODE):
+        super().__init__()
+        self.window = window
+        self.mode = mode
+
+    def extra_repr(self):
+        return f"window={self.window}, mode={self.mode}"
+
+    def time_warp(self, x):
+        org_size = x.size()
+        if x.dim() == 3:
+            # x: (Batch, Time, Freq) -> (Batch, 1, Time, Freq)
+            x = x[:, None]
+
+        t = x.shape[2]
+        if t - self.window <= self.window:
+            return x.view(*org_size)
+
+        center = torch.randint(self.window, t - self.window, (1,))[0]
+        warped = torch.randint(center - self.window, center + self.window, (1,))[0] + 1
+
+        # left: (Batch, Channel, warped, Freq)
+        # right: (Batch, Channel, time - warped, Freq)
+        left = torch.nn.functional.interpolate(
+            x[:, :, :center], (warped, x.shape[3]), mode=self.mode, align_corners=False
+        )
+        right = torch.nn.functional.interpolate(
+            x[:, :, center:], (t - warped, x.shape[3]), mode=self.mode, align_corners=False
+        )
+
+        if x.requires_grad:
+            x = torch.cat([left, right], dim=-2)
+        else:
+            x[:, :, :warped] = left
+            x[:, :, warped:] = right
+
+        return x.view(*org_size)
+
+    def forward(self, x, x_lengths=None):
+        """Forward function.
+        Args:
+            x: (Batch, Time, Freq)
+            x_lengths: (Batch,)
+        """
+        ys = x.new_zeros(x.size())
+        for i in range(x.size(0)):
+            _y = self.time_warp(
+                x[i][None, : x_lengths[i]],
+            )[0]
+            ys[i, : x_lengths[i]] = _y
+
+        return ys, x_lengths
+
+
+class MaskAlongAxis(torch.nn.Module):
+    def __init__(
+        self,
+        mask_width_range=(0, 30),
+        num_mask=2,
+        dim="time",
+        replace_with_zero=True,
+    ):
+        if isinstance(mask_width_range, int):
+            mask_width_range = (0, mask_width_range)
+        if len(mask_width_range) != 2:
+            raise TypeError(
+                f"mask_width_range must be a tuple of int and int values: "
+                f"{mask_width_range}",
+            )
+
+        assert mask_width_range[1] > mask_width_range[0]
+        if isinstance(dim, str):
+            if dim == "time":
+                dim = 1
+            elif dim == "freq":
+                dim = 2
+            else:
+                raise ValueError("dim must be int, 'time' or 'freq'")
+        if dim == 1:
+            self.mask_axis = "time"
+        elif dim == 2:
+            self.mask_axis = "freq"
+        else:
+            self.mask_axis = "unknown"
+
+        super().__init__()
+        self.mask_width_range = mask_width_range
+        self.num_mask = num_mask
+        self.dim = dim
+        self.replace_with_zero = replace_with_zero
+
+    def mask_along_axis(self, spec, spec_lengths):
+        org_size = spec.size()
+        if spec.dim() == 4:
+            # spec: (Batch, Channel, Length, Freq) -> (Batch * Channel, Length, Freq)
+            spec = spec.view(-1, spec.size(2), spec.size(3))
+
+        B = spec.shape[0]
+        # D = Length or Freq
+        D = spec.shape[self.dim]
+        # mask_length: (B, num_mask, 1)
+        mask_length = torch.randint(
+            self.mask_width_range[0],
+            self.mask_width_range[1],
+            (B, self.num_mask),
+            device=spec.device,
+        ).unsqueeze(2)
+
+        # mask_pos: (B, num_mask, 1)
+        mask_pos = torch.randint(
+            0, max(1, D - mask_length.max()), (B, self.num_mask), device=spec.device
+        ).unsqueeze(2)
+
+        # aran: (1, 1, D)
+        aran = torch.arange(D, device=spec.device)[None, None, :]
+        # mask: (Batch, num_mask, D)
+        mask = (mask_pos <= aran) * (aran < (mask_pos + mask_length))
+        # Multiply masks: (Batch, num_mask, D) -> (Batch, D)
+        mask = mask.any(dim=1)
+        if self.dim == 1:
+            # mask: (Batch, Length, 1)
+            mask = mask.unsqueeze(2)
+        elif self.dim == 2:
+            # mask: (Batch, 1, Freq)
+            mask = mask.unsqueeze(1)
+
+        if self.replace_with_zero:
+            value = 0.0
+        else:
+            value = spec.mean()
+
+        if spec.requires_grad:
+            spec = spec.masked_fill(mask, value)
+        else:
+            spec = spec.masked_fill_(mask, value)
+        spec = spec.view(*org_size)
+        return spec, spec_lengths
+
+    def forward(self, spec, spec_lengths=None):
+        """Forward function.
+        Args:
+            spec: (Batch, Length, Freq)
+        """
+
+        return self.mask_along_axis(
+            spec,
+            spec_lengths,
+        )
