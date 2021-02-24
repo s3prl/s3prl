f2d28076b8cb9a89f43132e99ded732405e03f8f
Yist Lin <yishen992@gmail.com>
2021-02-24 16:10:13 +0800

    use FC plus attentive pooling instead of LSTM

--- downstream/sws2013/model.py
+++ downstream/sws2013/model.py
@@ -4,27 +4,20 @@ import torch.nn.functional as F
 
 
 class Model(nn.Module):
-    def __init__(self, input_dim, **kwargs):
+    def __init__(self, input_dim, bottleneck_dim, hidden_dim, **kwargs):
         super(Model, self).__init__()
 
-        btn_dim = kwargs["bottleneck_dim"]
-        hid_dim = kwargs["hidden_dim"]
-        num_layers = kwargs["num_layers"]
-
-        self.connector = nn.Linear(input_dim, btn_dim)
-        self.rnn = nn.LSTM(
-            input_size=btn_dim,
-            hidden_size=hid_dim,
-            num_layers=num_layers,
-            batch_first=True,
-        )
-        self.attention_linear = nn.Linear(hid_dim, 1)
+        self.connector = nn.Linear(input_dim, bottleneck_dim)
+        self.fc1 = nn.Linear(bottleneck_dim, hidden_dim)
+        self.attention_linear = nn.Linear(hidden_dim, 1)
 
     def forward(self, features):
+        # transforming
         hiddens = F.relu(self.connector(features))
-        outputs, _ = self.rnn(hiddens)
-        embeds = torch.tanh(outputs)
-        attention_weights = F.softmax(self.attention_linear(embeds), dim=1)
-        embeds = torch.sum(embeds * attention_weights, dim=1)
-        embeds = embeds.div(embeds.norm(2, dim=-1, keepdim=True))
+        hiddens = torch.tanh(self.fc1(hiddens))
+
+        # attentive pooling
+        attention_weights = F.softmax(self.attention_linear(hiddens), dim=1)
+        embeds = torch.sum(hiddens * attention_weights, dim=1)
+
         return embeds
