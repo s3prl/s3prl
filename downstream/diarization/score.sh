34889c13cc6d85883b6b3447e3693b5fb1461a9b
Leo Yang <leo19941227@gmail.com>
2021-03-15 04:47:19 +0800

    minor debug for diarization scoring display

--- downstream/diarization/score.sh
+++ downstream/diarization/score.sh
@@ -43,8 +43,6 @@ for med in 1 11; do
         #     -s $scoring_dir/hyp_${th}_$med.rttm > $scoring_dir/result_th${th}_med${med}_collar0 2>/dev/null || exit
     done
 done
+
 grep OVER $scoring_dir/result_th0.[^_]*_med[^_]* \
-     | sort -nrk 7 
-# grep OVER $scoring_dir/result_th0.[^_]*_med[^_]* \
-#      | sort -nrk 4 \
-#      | tail -n 1 | awk -F '[[:space:]][[:space:]]+' '{print $1 "\t" $2}'
+     | sort -nrk 2
