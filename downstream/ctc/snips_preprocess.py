ca17e6e1a96fe4d0c3418c29d70ea34c916e13d2
Yung-Sung Chuang <b05901033@ntu.edu.tw>
2021-03-17 00:29:26 +0800

    delete comments

--- downstream/ctc/snips_preprocess.py
+++ downstream/ctc/snips_preprocess.py
@@ -4,7 +4,7 @@ from glob import glob
 import shutil
 import re
 import tqdm
-from multiprocessing import Process, Pool
+from multiprocessing import Pool
 
 from normalise import normalise
 
@@ -280,17 +280,6 @@ def sox_mp3_to_wav(in_root, out_root):
         print('[Processing] process %d audio files from speaker %s'%(len(files), speaker))
         inputs.append((files, root, out_root, speaker))
     pool.map(sox_func, inputs)
-    '''
-    for name in tqdm.tqdm(files):
-        if name.endswith(".mp3"):
-            split = name.split('-')[1]
-            out_dir = os.path.join(out_root, split)
-            os.makedirs(out_dir, exist_ok=True)
-            orig_file = os.path.join(root, name)
-            new_file  = os.path.join(out_dir, speaker+'-'+name.split('/')[-1].split('.')[0] + '.wav')
-            bashCommand = "sox " + orig_file + " -t wav -c 1 -r 16000 -b 16 -e signed-integer " + new_file
-            r = os.popen(bashCommand).read()
-    '''
 
 if __name__ == '__main__':
 
