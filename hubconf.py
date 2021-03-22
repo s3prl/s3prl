c1765b9f16cd7ce3252d26d89415ec0bd92344b4
Leo Yang <leo19941227@gmail.com>
2021-03-22 09:29:57 +0800

    solve rglob too slow in root hubconf

--- hubconf.py
+++ hubconf.py
@@ -16,9 +16,8 @@ dependencies = ['torch']
 
 
 search_root = os.path.dirname(__file__)
-hubconfs = [str(p) for p in pathlib.Path(search_root).rglob('hubconf.py')]
+hubconfs = [str(p) for p in pathlib.Path(search_root).glob('*/*/hubconf.py')]
 hubconfs = [os.path.relpath(p, search_root) for p in hubconfs]
-hubconfs.remove('hubconf.py')  # remove the root hubconf.py
 
 for hubconf in hubconfs:
     module_name = '.'.join(str(hubconf).split('.')[:-1]).replace('/', '.')
