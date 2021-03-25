7722356bd11963f392c52ef8a8ff9e9bdcb89dfc
andi611 <tingweiandyliu@gmail.com>
2021-03-25 23:08:29 +0800

    add stop step for search

--- utility/get_best_dev.py
+++ utility/get_best_dev.py
@@ -10,6 +10,8 @@
 """
 Usage:
     `python utility/get_best_dev.py result/downstream/example/log.log`
+    `python utility/get_best_dev.py result/downstream/example/log.log $stop_step`
+    where $stop_step is an int that specifies the maximum log step to search for.
 """
 
 
@@ -26,6 +28,11 @@ def main():
 
     log_file = str(sys.argv[1])
 
+    if len(sys.argv) == 3:
+        stop_step = int(sys.argv[2])
+    else:
+        stop_step = 99999999
+
     best_dev, best_step, best_test = None, None, None
 
     with open(log_file) as f:
@@ -41,6 +48,9 @@ def main():
             if line[0].lower() == 'test':
                 if line[-2].strip(':') == best_step:
                     best_test = line[-1]
+
+            if int(line[-2].strip(':')) > stop_step:
+                break
     
     print(f'The best dev score {best_dev} at step {best_step}, accoupanied by this test score {best_test}')
 
