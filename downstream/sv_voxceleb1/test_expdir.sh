973e0d500e8edf59cfbb8622ab8e059690a462f1
Leo Yang <leo19941227@gmail.com>
2021-03-27 13:45:57 +0000

    update sv test script

--- downstream/sv_voxceleb1/test_expdir.sh
+++ downstream/sv_voxceleb1/test_expdir.sh
@@ -19,8 +19,12 @@ echo Start evaluaing ckpts...
 for state_name in 20000 40000 60000 80000 100000 120000 140000 160000 180000 200000;
 do
     ckpt_path="$expdir/states-$state_name.ckpt"
+    if [ ! -f "$ckpt_path" ]; then
+        continue
+    fi
+
     log_path="$expdir/states-$state_name.result"
-    if [ ! -f "$log_path" ] || [ "$(cat $log_path | wc -l)" -lt 1 ]; then
+    if [ ! -f "$log_path" ] || [ "$(cat "$log_path" | grep "test-ERR" | wc -l)" -lt 1 ]; then
         eval "$commands -e $ckpt_path" > $log_path
     fi
 done
