6242b7bef7c43dbd8d089dd70ac717913d971cd5
Leo Yang <leo19941227@gmail.com>
2021-05-22 21:14:41 +0800

    fix mockingjay hook bug on inter infer

--- upstream/mockingjay/expert.py
+++ upstream/mockingjay/expert.py
@@ -7,7 +7,12 @@
 """*********************************************************************************************"""
 
 
+from typing import List, Tuple
+from collections import OrderedDict
+
 import yaml
+import torch
+from torch.tensor import Tensor
 
 from upstream.interfaces import UpstreamBase
 from .builder import PretrainedTransformer
@@ -49,10 +54,31 @@ class UpstreamExpert(UpstreamBase):
         ), "This wrapper only supports `on-the-fly` ckpt with built in feature extracters."
 
         if len(self.hooks) == 0:
-            for block_id, _ in enumerate(self.transformer.model.encoder.layer):
-                self.hooks[
-                    f"self.transformer.model.encoder.layer[{block_id}]"
-                ] = lambda input, output: output
+            encoder_path = "self.transformer.model.encoder"
+
+            for i in range(len(self.transformer.model.encoder.layer)):
+                self.add_hook(f"{encoder_path}.layer[{i}]", lambda i, o: i[0])
+            self.add_hook(
+                encoder_path, lambda i, o: o[1][0] if isinstance(o, tuple) else o[0]
+            )
+
+            def hook_postprocess(
+                hiddens: List[Tuple[str, Tensor]]
+            ) -> List[Tuple[str, Tensor]]:
+                updated_hiddens_dict = OrderedDict()
+                for identifier, tensor in hiddens:
+                    if not identifier in updated_hiddens_dict:
+                        updated_hiddens_dict[identifier] = [tensor]
+                    else:
+                        updated_hiddens_dict[identifier].append(tensor)
+
+                updated_hiddens = []
+                for identifier, tensors in updated_hiddens_dict.items():
+                    updated_hiddens.append((identifier, torch.cat(tensors, dim=1)))
+
+                return updated_hiddens
+
+            self.hook_postprocess = hook_postprocess
 
     def forward(self, wavs):
         features = self.transformer(wavs)  # (batch_size, extracted_seqlen, feature_dim)
