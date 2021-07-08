f29de0a014c01af8a4824a55ba6508a8d0fa9419
RayTzeng <wiz94156.eed06@nctu.edu.tw>
2021-07-08 11:36:57 +0800

    remove bash file and requirements for mos prediction downstream task

--- train_wav2MOS.sh
+++
@@ -1,8 +0,0 @@
-# name of the experiment
-EXP_NAME=wav2MOS_segment_cpc_testing
-# which upstream to run. Ex. wav2vec2, npc, TERA, ...
-UPSTREAM=cpc
-# which downstream to run.
-DOWNSTREAM=mos_prediction_segment
-
-python3 run_downstream.py -m train -n $EXP_NAME -u $UPSTREAM -d $DOWNSTREAM
