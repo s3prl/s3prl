e3355c6af45e16cc9334f179a3c1dd2398d628d6
Leo Yang <leo19941227@gmail.com>
2021-07-14 22:49:19 +0800

    use archieve file instead of git protocol for fairseq

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ requirements = [
     "editdistance",
     "easydict",
     "catalyst",
-    "fairseq@git+https://github.com//pytorch/fairseq.git@f2146bdc7abf293186de9449bfa2272775e39e1d#egg=fairseq",
+    "fairseq @ https://github.com/pytorch/fairseq/archive/f2146bdc7abf293186de9449bfa2272775e39e1d.zip",
 ]
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -68,7 +68,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',  # Required
+    version='0.1.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
