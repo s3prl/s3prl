47672a188a38bddb2a2f764ff070a4b8b0478f06
Leo Yang <leo19941227@gmail.com>
2021-09-24 02:07:16 +0800

    minor refactor on hub

--- s3prl/hub.py
+++ s3prl/hub.py
@@ -1,3 +1,4 @@
+import os as _os
 import pathlib as _pathlib
 import importlib as _importlib
 
@@ -6,13 +7,13 @@ _hubconfs = list(_search_root.glob("upstream/*/hubconf.py"))
 _hubconfs += list(_search_root.glob("downstream/*/hubconf.py"))
 
 for _hubconf in _hubconfs:
-    posixpath = _hubconf.relative_to(_search_root).as_posix()
+    relpath = _hubconf.relative_to(_search_root)
     try:
-        _module_name = "." + ".".join(posixpath.split(".")[:-1]).replace("/", ".")
+        _module_name = "." + str(relpath).replace(_os.path.sep, ".")[:-3]  # remove .py
         _module = _importlib.import_module(_module_name, package=__package__)
 
     except ModuleNotFoundError as e:
-        print(f'[Warning] can not import {_module_name}: {str(e)}. Please see {_pathlib.Path(posixpath).parent / "README.md"}')
+        print(f'[Warning] can not import {_module_name}: {str(e)}. Please see {relpath.parent / "README.md"}')
         continue
 
     for variable_name in dir(_module):
