9fb853c27ee051cdcb2297e30ae1502486ac5908
Leo Yang <leo19941227@gmail.com>
2021-10-15 21:08:11 -0400

    revision support for huggingface hub

--- s3prl/run_downstream.py
+++ s3prl/run_downstream.py
@@ -69,7 +69,8 @@ def get_downstream_args():
     parser.add_argument('-f', '--upstream_trainable', action='store_true', help='Fine-tune, set upstream.train(). Default is upstream.eval()')
     parser.add_argument('-s', '--upstream_feature_selection', default='hidden_states', help='Specify the layer to be extracted as the representation')
     parser.add_argument('-l', '--upstream_layer_selection', type=int, help='Select a specific layer for the features selected by -s')
-    parser.add_argument('--upstream_model_name', default="model.pt", help='The name of the model file in the Hugging Face Hub repo.')
+    parser.add_argument('--upstream_model_name', default="model.pt", help='The name of the model file in the HuggingFace Hub repo.')
+    parser.add_argument('--upstream_revision', help="The commit hash of the specified HuggingFace Repository")
 
     # experiment directory, choose one to specify
     # expname uses the default root directory: result/downstream
