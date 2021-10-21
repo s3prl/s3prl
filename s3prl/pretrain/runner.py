461703aad709efeffac0292519c2a78393861e47
vectominist <b06901020@ntu.edu.tw>
2021-10-21 16:49:41 +0800

    remove formatting to match orginal repo

--- s3prl/pretrain/runner.py
+++ s3prl/pretrain/runner.py
@@ -17,14 +17,12 @@ import random
 import importlib
 from tqdm import tqdm
 from collections import defaultdict
-
-# -------------#
+#-------------#
 import torch
 import torch.nn as nn
 from tensorboardX import SummaryWriter
 import numpy as np
-
-# -------------#
+#-------------#
 from s3prl.optimizers import get_optimizer
 from s3prl.schedulers import get_scheduler
 
@@ -32,86 +30,81 @@ from s3prl.schedulers import get_scheduler
 ##########
 # RUNNER #
 ##########
-class Runner:
+class Runner():
     """
     Used to handle high-level concepts of a ML experiment
     eg. training loop, evaluation loop, upstream propagation, optimization, tensorboard logging, checkpoint saving
     """
-
     def __init__(self, args, config):
         self.args = args
         self.config = config
-        self.logger = SummaryWriter(args.expdir)
+        self.logger = SummaryWriter(args.expdir)                                                 
 
-        self.init_ckpt = (
-            torch.load(self.args.past_exp, map_location="cpu")
-            if self.args.past_exp
-            else {}
-        )
+        self.init_ckpt = torch.load(self.args.past_exp, map_location='cpu') if self.args.past_exp else {}
         self.upstream = self._get_upstream()
         self.amp = self.config["runner"]["fp16"]
         self.prev_nan = False
 
     def _get_upstream(self):
-        init_upstream = self.init_ckpt.get("Config")
+        init_upstream = self.init_ckpt.get('Config')
         if init_upstream:
             self.args.upstream_config = init_upstream
-        module_path = f"pretrain.{self.args.upstream}.pretrain_expert"
-        Upstream = getattr(
-            importlib.import_module(module_path), "UpstreamPretrainExpert"
-        )
-        upstream = Upstream(
-            self.config["pretrain_expert"]["datarc"],
-            self.args.upstream_config,
-            self.args.device,
-            self.args.multi_gpu,
-        ).to(self.args.device)
+        module_path = f'pretrain.{self.args.upstream}.pretrain_expert'
+        Upstream = getattr(importlib.import_module(module_path), 'UpstreamPretrainExpert')
+        upstream = Upstream(self.config['pretrain_expert']['datarc'], 
+                            self.args.upstream_config,
+                            self.args.device,
+                            self.args.multi_gpu).to(self.args.device)
 
-        assert hasattr(upstream, "device")
-        assert hasattr(upstream, "forward")
-        assert hasattr(upstream, "load_model")
-        assert hasattr(upstream, "add_state_to_save")
-        assert hasattr(upstream, "on_before_zero_grad")
-        assert hasattr(upstream, "get_train_dataloader")
+        assert hasattr(upstream, 'device')
+        assert hasattr(upstream, 'forward')
+        assert hasattr(upstream, 'load_model')
+        assert hasattr(upstream, 'add_state_to_save')
+        assert hasattr(upstream, 'on_before_zero_grad')
+        assert hasattr(upstream, 'get_train_dataloader')
 
         if self.init_ckpt != {}:
-            print("[Runner] - Loading upstream weights from the previous experiment")
+            print('[Runner] - Loading upstream weights from the previous experiment')
             upstream.load_model(self.init_ckpt)
         return upstream
 
+
     def _get_optimizer(self, model_params):
         optimizer = get_optimizer(
-            model_params, self.config["runner"]["total_steps"], self.config["optimizer"]
+            model_params, 
+            self.config['runner']['total_steps'],
+            self.config['optimizer']
         )
 
-        init_optimizer = self.init_ckpt.get("Optimizer")
+        init_optimizer = self.init_ckpt.get('Optimizer')
         if init_optimizer:
-            print("[Runner] - Loading optimizer weights from the previous experiment")
+            print('[Runner] - Loading optimizer weights from the previous experiment')
             optimizer.load_state_dict(init_optimizer)
         return optimizer
 
+
     def _get_scheduler(self, optimizer):
         scheduler = get_scheduler(
-            optimizer, self.config["runner"]["total_steps"], self.config["scheduler"]
+            optimizer,
+            self.config['runner']['total_steps'],
+            self.config['scheduler']
         )
 
-        init_scheduler = self.init_ckpt.get("Scheduler")
+        init_scheduler = self.init_ckpt.get('Scheduler')
         if init_scheduler:
-            print("[Runner] - Loading scheduler weights from the previous experiment")
+            print('[Runner] - Loading scheduler weights from the previous experiment')
             scheduler.load_state_dict(init_scheduler)
         return scheduler
+
 
     def train(self):
         # set model train mode
         self.upstream.train()
 
         # prepare data
-        gradient_accumulate_steps = self.config["runner"]["gradient_accumulate_steps"]
-        print(
-            "[Runner] - Accumulated batch size:",
-            self.config["pretrain_expert"]["datarc"]["train_batch_size"]
-            * gradient_accumulate_steps,
-        )
+        gradient_accumulate_steps = self.config['runner']['gradient_accumulate_steps']
+        print('[Runner] - Accumulated batch size:', 
+              self.config['pretrain_expert']['datarc']['train_batch_size'] * gradient_accumulate_steps)
         dataloader = self.upstream.get_train_dataloader()
 
         # set epoch
@@ -128,32 +121,33 @@ class Runner:
         assert self.config['runner']['total_steps'] > self.config['runner']['log_step']
         assert self.config['runner']['total_steps'] > self.config['runner']['save_step']
 
+        # set amp
+        if self.amp:
+            print('[Runner] - Enabled fp16 training')
+            scaler = torch.cuda.amp.GradScaler()
+
         # set optimizer
         model_params = [self.upstream]
         optimizer = self._get_optimizer(model_params)
 
         # set scheduler
         scheduler = None
-        if self.config.get("scheduler"):
+        if self.config.get('scheduler'):
             scheduler = self._get_scheduler(optimizer)
 
         # set progress bar
-        pbar = tqdm(
-            total=self.config["runner"]["total_steps"],
-            dynamic_ncols=True,
-            desc="overall",
-        )
-        init_step = self.init_ckpt.get("Step")
+        pbar = tqdm(total=self.config['runner']['total_steps'], dynamic_ncols=True, desc='overall')
+        init_step = self.init_ckpt.get('Step')
         if init_step:
             pbar.n = init_step
 
         all_loss = 0
         backward_steps = 0
         records = defaultdict(list)
-        prefix = f"{self.args.upstream}/train-"
+        prefix = f'{self.args.upstream}/train-'
 
         while pbar.n < pbar.total:
-            for data in tqdm(dataloader, dynamic_ncols=True, desc="train"):
+            for data in tqdm(dataloader, dynamic_ncols=True, desc='train'):
                 # try/except block for forward/backward
                 try:
                     if pbar.n >= pbar.total:
@@ -186,10 +180,11 @@ class Runner:
                         loss.backward()
 
                 except RuntimeError as e:
-                    if "CUDA out of memory" in str(e):
-                        print(f"[Runner] - CUDA out of memory at step {global_step}")
+                    if 'CUDA out of memory' in str(e):
+                        print(f'[Runner] - CUDA out of memory at step {global_step}')
                         torch.cuda.empty_cache()
                         optimizer.zero_grad()
+                        self.prev_nan = False
                         continue
                     else:
                         raise
@@ -197,26 +192,23 @@ class Runner:
                 # record loss
                 all_loss += loss.item()
                 del loss
-
+                
                 # whether to accumulate gradient
                 backward_steps += 1
                 if backward_steps % gradient_accumulate_steps > 0:
                     continue
-
+                    
                 # unscale
                 if self.amp and not self.prev_nan:
                     scaler.unscale_(optimizer)
 
                 # gradient clipping
-                grad_norm = torch.nn.utils.clip_grad_norm_(
-                    self.upstream.model.parameters(),
-                    self.config["runner"]["gradient_clipping"],
-                )
+                grad_norm = torch.nn.utils.clip_grad_norm_(self.upstream.model.parameters(), self.config['runner']['gradient_clipping'])
 
                 # optimize
                 if math.isnan(grad_norm):
                     self.prev_nan = True
-                    print(f"[Runner] - Error : grad norm is NaN at step {global_step}")
+                    print(f'[Runner] - Error : grad norm is NaN at step {global_step}')
                 else:
                     self.prev_nan = False
                     if self.amp:
@@ -224,7 +216,7 @@ class Runner:
                         scaler.update()
                     else:
                         optimizer.step()
-
+                    
                 self.upstream.on_before_zero_grad()
                 optimizer.zero_grad()
 
@@ -233,31 +225,16 @@ class Runner:
                     scheduler.step()
 
                 # logging
-                if (
-                    global_step % self.config["runner"]["log_step"] == 0
-                    or pbar.n == pbar.total - 1
-                ):
+                if global_step % self.config['runner']['log_step'] == 0 or pbar.n == pbar.total -1:
                     # log loss
-                    self.logger.add_scalar(
-                        f"{prefix}loss", all_loss, global_step=global_step
-                    )
+                    self.logger.add_scalar(f'{prefix}loss', all_loss, global_step=global_step)
                     # log lr
-                    if hasattr(optimizer, "get_lr"):
-                        self.logger.add_scalar(
-                            f"{prefix}lr",
-                            optimizer.get_lr()[0],
-                            global_step=global_step,
-                        )
+                    if hasattr(optimizer, 'get_lr'):
+                        self.logger.add_scalar(f'{prefix}lr', optimizer.get_lr()[0], global_step=global_step)
                     else:
-                        self.logger.add_scalar(
-                            f"{prefix}lr",
-                            self.config["optimizer"]["lr"],
-                            global_step=global_step,
-                        )
+                        self.logger.add_scalar(f'{prefix}lr', self.config['optimizer']['lr'], global_step=global_step)
                     # log norm
-                    self.logger.add_scalar(
-                        f"{prefix}gradient norm", grad_norm, global_step=global_step
-                    )
+                    self.logger.add_scalar(f'{prefix}gradient norm', grad_norm, global_step=global_step)
 
                     # log customized contents
                     self.upstream.log_records(
@@ -268,45 +245,34 @@ class Runner:
                     )
                     records = defaultdict(list)
 
-                if (
-                    global_step % self.config["runner"]["save_step"] == 0
-                    or pbar.n == pbar.total - 1
-                ):
-
+                if global_step % self.config['runner']['save_step'] == 0 or pbar.n == pbar.total -1:
                     def check_ckpt_num(directory):
-                        max_keep = self.config["runner"]["max_keep"]
-                        ckpt_pths = glob.glob(f"{directory}/states-*.ckpt")
+                        max_keep = self.config['runner']['max_keep']
+                        ckpt_pths = glob.glob(f'{directory}/states-*.ckpt')
                         if len(ckpt_pths) >= max_keep:
-                            ckpt_pths = sorted(
-                                ckpt_pths,
-                                key=lambda pth: int(pth.split("-")[-1].split(".")[0]),
-                            )
-                            for ckpt_pth in ckpt_pths[: len(ckpt_pths) - max_keep + 1]:
+                            ckpt_pths = sorted(ckpt_pths, key=lambda pth: int(pth.split('-')[-1].split('.')[0]))
+                            for ckpt_pth in ckpt_pths[:len(ckpt_pths) - max_keep + 1]:
                                 os.remove(ckpt_pth)
-
                     check_ckpt_num(self.args.expdir)
 
                     all_states = {
-                        "Optimizer": optimizer.state_dict(),
-                        "Step": global_step,
-                        "Args": self.args,
-                        "Runner": self.config,
+                        'Optimizer': optimizer.state_dict(),
+                        'Step': global_step,
+                        'Args': self.args,
+                        'Runner': self.config,
                     }
                     all_states = self.upstream.add_state_to_save(all_states)
 
                     if scheduler:
-                        all_states["Scheduler"] = scheduler.state_dict()
-
-                    name = (
-                        f"states-epoch-{n_epochs}.ckpt"
-                        if pbar.n == pbar.total - 1 and n_epochs > 0
-                        else f"states-{global_step}.ckpt"
-                    )
+                        all_states['Scheduler'] = scheduler.state_dict()
+                    
+                    name = f'states-epoch-{n_epochs}.ckpt' if pbar.n == pbar.total -1 and n_epochs > 0 else \
+                           f'states-{global_step}.ckpt'
                     save_path = os.path.join(self.args.expdir, name)
-                    tqdm.write(f"[Runner] - Save the checkpoint to: {save_path}")
+                    tqdm.write(f'[Runner] - Save the checkpoint to: {save_path}')
                     torch.save(all_states, save_path)
-
-                all_loss = 0
+                
+                all_loss = 0      
                 pbar.update(1)
 
         pbar.close()
