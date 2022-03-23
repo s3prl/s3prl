345de048ae215beb8d32e9f58832d1d7010a1e5f
Leo Yang <leo19941227@gmail.com>
2022-03-24 03:24:36 +0800

    solve class alising

--- s3prl/superb/sid/__init__.py
+++ s3prl/superb/sid/__init__.py
@@ -2,7 +2,7 @@ from s3prl.task import UtteranceClassification as Task
 from s3prl.nn import MeanPoolingLinear as DownstreamModel
 
 from s3prl.dataset import UtteranceClassificationDataset as TrainDataset
-ValidDataset = TrainDataset
-TestDataset = TrainDataset
+from s3prl.dataset import UtteranceClassificationDataset as ValidDataset
+from s3prl.dataset import UtteranceClassificationDataset as TestDataset
 
 from s3prl.preprocessor import VoxCeleb1SIDPreprocessor as Preprocessor
