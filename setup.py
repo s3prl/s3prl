98947b29b6352d312996181438568e3435690af3
Heng-Jui Chang <b06901020@ntu.edu.tw>
2022-04-18 10:27:13 +0800

    fixed package installation error

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ requirements = [
     "easydict",
     "catalyst",
     "sentencepiece",
-    "python-dotenv"
-    "huggingface_hub @ git+https://github.com/huggingface/huggingface_hub.git#egg=huggingface_hub" # TODO: Replace with v0.0.17 when it is released
+    "python-dotenv",
+    "huggingface_hub" # TODO: Replace with v0.0.17 when it is released
 ]
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
