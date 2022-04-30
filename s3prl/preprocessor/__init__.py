7042d5356651c24539faf3e45858a395c1b33144
Leo Yang <leo19941227@gmail.com>
2022-04-30 15:51:20 +0800

    fix deprecated packages import error

--- s3prl/preprocessor/__init__.py
+++ s3prl/preprocessor/__init__.py
@@ -1,5 +1,4 @@
 from .example_utterance_classification_preprocessor import (
     ExampleUtteranceClassificationPreprocessor,
 )
-from .librispeech_ctc_preprocessor import LibriSpeechCTCPreprocessor
 from .voxceleb1_sid_preprocessor import VoxCeleb1SIDPreprocessor
