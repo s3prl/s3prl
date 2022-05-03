1c11dcb4715095556e8056b65dae0012efe9067e
Leo Yang <leo19941227@gmail.com>
2022-05-03 20:41:41 +0800

    a more useful benchmark

--- s3prl/util/benchmark.py
+++ s3prl/util/benchmark.py
@@ -1,19 +1,28 @@
 import logging
-from contextlib import ContextDecorator
+import numpy as np
 from time import time
 from typing import Any
+from contextlib import ContextDecorator
 
 logger = logging.getLogger(__name__)
 
 
 class benchmark(ContextDecorator):
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, freq: int = 20) -> None:
         super().__init__()
         self.name = name
+        self.freq = freq
+        self.history = []
+        self.n = 0
 
     def __enter__(self):
-        logger.info(f"{self.name}: start")
         self.start = time()
+        self.n += 1
 
     def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
-        logger.info(f"{self.name}: finish with {time() - self.start} seconds")
+        seconds = time() - self.start
+        self.history.append(seconds)
+        if self.n % self.freq == 0:
+            logger.info(
+                f"{self.name}: {seconds} secs, avg {np.array(self.history).mean()} secs"
+            )
