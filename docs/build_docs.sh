ecf64da16d42c8b6c85b46d2a58435ee4bed0833
Leo Yang <leo19941227@gmail.com>
2022-05-16 04:00:13 +0800

    simplify sphinx-apidoc

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -5,9 +5,8 @@
 ignore_paths=$(python3 util/is_valid.py ../s3prl ../valid_paths.txt)
 
 cd ../
-sphinx-apidoc -d 3 --separate --implicit-namespace -o ./docs/source s3prl $ignore_paths
+sphinx-apidoc -o ./docs/source s3prl $ignore_paths
 cd docs
 
 make clean
 make html
-
