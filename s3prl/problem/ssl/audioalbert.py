4cd74b11bb4e18015b527817a049b7ee41c35741
andi611 <tingweiandyliu@gmail.com>
2022-05-16 23:02:47 +0800

    apply black format

--- s3prl/problem/ssl/audioalbert.py
+++ s3prl/problem/ssl/audioalbert.py
@@ -10,7 +10,7 @@ from s3prl.task.feat_reconstruction_task import FeatReconstructionTask
 from s3prl.nn.transformer_audioalbert import (
     TransformerConfig,
     TransformerModel,
-    TransformerSpecPredictionHead
+    TransformerSpecPredictionHead,
 )
 from torch.nn import L1Loss
 from s3prl import Container
@@ -35,31 +35,35 @@ class AudioAlbert:
             train_split=["train-clean-100", "train-clean-360", "train-other-500"]
         ),
         TrainData=dict(
-            mask_args = dict(
-                position_encoding_size=768,    # int, this should be identical to `hidden_size`
-                mask_proportion=0.15,          # float, mask this percentage of all spectrogram frames in each sequence at random during MAM training
-                mask_consecutive_min=7,        # int, mask this amount of consecutive frames
-                mask_consecutive_max=7,        # int, mask this amount of consecutive frames
-                mask_allow_overlap=True,       # bool, allow overlap masking
-                mask_bucket_ratio=1.5,         # float, only used when overlap is not allowed. sample a mask from each bucket in size of [sampled mask_consecutive * mask_bucket_ratio]
-                mask_frequency=0,              # int, mask maximum this percentage of frequency bands, set to 0 for no frequency mask  
+            mask_args=dict(
+                position_encoding_size=768,  # int, this should be identical to `hidden_size`
+                mask_proportion=0.15,  # float, mask this percentage of all spectrogram frames in each sequence at random during MAM training
+                mask_consecutive_min=7,  # int, mask this amount of consecutive frames
+                mask_consecutive_max=7,  # int, mask this amount of consecutive frames
+                mask_allow_overlap=True,  # bool, allow overlap masking
+                mask_bucket_ratio=1.5,  # float, only used when overlap is not allowed. sample a mask from each bucket in size of [sampled mask_consecutive * mask_bucket_ratio]
+                mask_frequency=0,  # int, mask maximum this percentage of frequency bands, set to 0 for no frequency mask
             ),
-            audio_config = dict(
+            audio_config=dict(
                 win_ms=25,
                 hop_ms=10,
                 n_freq=201,
                 n_mels=80,
                 n_mfcc=13,
-                input = {'channel': 0,
-                        'cmvn': True,
-                        'delta': 0,
-                        'feat_type': 'mel',
-                        'log': True},
-                target = {'channel': 1,
-                        'cmvn': True,
-                        'delta': 0,
-                        'feat_type': 'mel',
-                        'log': True},
+                input={
+                    "channel": 0,
+                    "cmvn": True,
+                    "delta": 0,
+                    "feat_type": "mel",
+                    "log": True,
+                },
+                target={
+                    "channel": 1,
+                    "cmvn": True,
+                    "delta": 0,
+                    "feat_type": "mel",
+                    "log": True,
+                },
             ),
             target_level=-25,
         ),
@@ -76,27 +80,27 @@ class AudioAlbert:
             batch_size=2,
         ),
         ModelConfig=dict(
-            hidden_size=768,                        # Size of the encoder layers and the pooler layer.
-            num_hidden_layers=3,                    # Number of hidden layers in the Transformer encoder.
-            num_attention_heads=12,                 # Number of attention heads for each attention layer in the Transformer encoder.
-            intermediate_size=3072,                 # The size of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-            hidden_act="gelu",                      # The non-linear activation function (function or string) in the encoder and pooler. If string, "gelu", "relu" and "swish" are supported.
-            hidden_dropout_prob=0.1,                # The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
-            attention_probs_dropout_prob=0.1,       # The dropout ratio for the attention probabilities.
-            initializer_range=0.02,                 # The sttdev of the truncated_normal_initializer for initializing all weight matrices.
-            layer_norm_eps=1.e-12,                  # The epsilon used by LayerNorm.
-            share_layer=True,                       # Share layer weights
-            pre_layer_norm=False,                   # To apply the pre layer normalization technique introduced in: https://arxiv.org/abs/2002.04745
+            hidden_size=768,  # Size of the encoder layers and the pooler layer.
+            num_hidden_layers=3,  # Number of hidden layers in the Transformer encoder.
+            num_attention_heads=12,  # Number of attention heads for each attention layer in the Transformer encoder.
+            intermediate_size=3072,  # The size of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+            hidden_act="gelu",  # The non-linear activation function (function or string) in the encoder and pooler. If string, "gelu", "relu" and "swish" are supported.
+            hidden_dropout_prob=0.1,  # The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
+            attention_probs_dropout_prob=0.1,  # The dropout ratio for the attention probabilities.
+            initializer_range=0.02,  # The sttdev of the truncated_normal_initializer for initializing all weight matrices.
+            layer_norm_eps=1.0e-12,  # The epsilon used by LayerNorm.
+            share_layer=True,  # Share layer weights
+            pre_layer_norm=False,  # To apply the pre layer normalization technique introduced in: https://arxiv.org/abs/2002.04745
         ),
         Body=dict(
             input_dim=80,
-            output_attentions=False, 
-            keep_multihead_output=False, 
-            with_input_module=True
+            output_attentions=False,
+            keep_multihead_output=False,
+            with_input_module=True,
         ),
         Head=dict(
             output_dim=80,
-            input_dim=None, # automatically use hidden_state
+            input_dim=None,  # automatically use hidden_state
         ),
         Task=dict(),
         Loss=dict(),
--- s3prl/problem/ssl/mockingjay.py
+++ s3prl/problem/ssl/mockingjay.py
@@ -10,7 +10,7 @@ from s3prl.task.feat_reconstruction_task import FeatReconstructionTask
 from s3prl.nn.transformer_mockingjay import (
     TransformerConfig,
     TransformerModel,
-    TransformerSpecPredictionHead
+    TransformerSpecPredictionHead,
 )
 from torch.nn import L1Loss
 from s3prl import Container
@@ -35,37 +35,30 @@ class Mockingjay:
             train_split=["train-clean-100", "train-clean-360", "train-other-500"]
         ),
         TrainData=dict(
-            mask_args = dict(
-                position_encoding_size=768,    # int, this should be identical to `hidden_size`
-                mask_proportion=0.15,          # float, mask this percentage of all spectrogram frames in each sequence at random during MAM training
-                mask_consecutive_min=7,        # int, mask this amount of consecutive frames
-                mask_consecutive_max=7,        # int, mask this amount of consecutive frames
-                mask_allow_overlap=True,       # bool, allow overlap masking
-                mask_bucket_ratio=1.5,         # float, only used when overlap is not allowed. sample a mask from each bucket in size of [sampled mask_consecutive * mask_bucket_ratio]
-                mask_frequency=0,              # int, mask maximum this percentage of frequency bands, set to 0 for no frequency mask  
+            mask_args=dict(
+                position_encoding_size=768,  # int, this should be identical to `hidden_size`
+                mask_proportion=0.15,  # float, mask this percentage of all spectrogram frames in each sequence at random during MAM training
+                mask_consecutive_min=7,  # int, mask this amount of consecutive frames
+                mask_consecutive_max=7,  # int, mask this amount of consecutive frames
+                mask_allow_overlap=True,  # bool, allow overlap masking
+                mask_bucket_ratio=1.5,  # float, only used when overlap is not allowed. sample a mask from each bucket in size of [sampled mask_consecutive * mask_bucket_ratio]
+                mask_frequency=0,  # int, mask maximum this percentage of frequency bands, set to 0 for no frequency mask
             ),
-            audio_config = dict(
-                kaldi = { 
+            audio_config=dict(
+                kaldi={
                     "feat_type": "fbank",
                     "fbank": {
-                            "frame_length": 25.0,
-                            "frame_shift": 10.0,
-                            "num_mel_bins": 80,
-                            "use_log_fbank": True
+                        "frame_length": 25.0,
+                        "frame_shift": 10.0,
+                        "num_mel_bins": 80,
+                        "use_log_fbank": True,
                     },
-                    "mfcc": {
-                            "frame_length": 25.0,
-                            "frame_shift": 10.0,
-                            "num_ceps": 13
-                    },
-                    "spectrogram": {
-                        "frame_length": 25.0, 
-                        "frame_shift": 10.0
-                    }
+                    "mfcc": {"frame_length": 25.0, "frame_shift": 10.0, "num_ceps": 13},
+                    "spectrogram": {"frame_length": 25.0, "frame_shift": 10.0},
                 },
-                delta = {"order": 2, "win_length": 5},
-                cmvn = {"use_cmvn": True},
-            )
+                delta={"order": 2, "win_length": 5},
+                cmvn={"use_cmvn": True},
+            ),
         ),
         TrainSampler=dict(
             max_timestamp=16000 * 20,
@@ -80,27 +73,27 @@ class Mockingjay:
             batch_size=2,
         ),
         ModelConfig=dict(
-            hidden_size=768,                        # Size of the encoder layers and the pooler layer.
-            num_hidden_layers=3,                    # Number of hidden layers in the Transformer encoder.
-            num_attention_heads=12,                 # Number of attention heads for each attention layer in the Transformer encoder.
-            intermediate_size=3072,                 # The size of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-            hidden_act="gelu",                      # The non-linear activation function (function or string) in the encoder and pooler. If string, "gelu", "relu" and "swish" are supported.
-            hidden_dropout_prob=0.1,                # The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
-            attention_probs_dropout_prob=0.1,       # The dropout ratio for the attention probabilities.
-            initializer_range=0.02,                 # The sttdev of the truncated_normal_initializer for initializing all weight matrices.
-            layer_norm_eps=1.e-12,                  # The epsilon used by LayerNorm.
-            share_layer=False,                      # Share layer weights
-            pre_layer_norm=False,                   # To apply the pre layer normalization technique introduced in: https://arxiv.org/abs/2002.04745
+            hidden_size=768,  # Size of the encoder layers and the pooler layer.
+            num_hidden_layers=3,  # Number of hidden layers in the Transformer encoder.
+            num_attention_heads=12,  # Number of attention heads for each attention layer in the Transformer encoder.
+            intermediate_size=3072,  # The size of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+            hidden_act="gelu",  # The non-linear activation function (function or string) in the encoder and pooler. If string, "gelu", "relu" and "swish" are supported.
+            hidden_dropout_prob=0.1,  # The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
+            attention_probs_dropout_prob=0.1,  # The dropout ratio for the attention probabilities.
+            initializer_range=0.02,  # The sttdev of the truncated_normal_initializer for initializing all weight matrices.
+            layer_norm_eps=1.0e-12,  # The epsilon used by LayerNorm.
+            share_layer=False,  # Share layer weights
+            pre_layer_norm=False,  # To apply the pre layer normalization technique introduced in: https://arxiv.org/abs/2002.04745
         ),
         Body=dict(
             input_dim=240,
-            output_attentions=False, 
-            keep_multihead_output=False, 
-            with_input_module=True
+            output_attentions=False,
+            keep_multihead_output=False,
+            with_input_module=True,
         ),
         Head=dict(
             output_dim=240,
-            input_dim=None, # automatically use hidden_state
+            input_dim=None,  # automatically use hidden_state
         ),
         Task=dict(),
         Loss=dict(),
