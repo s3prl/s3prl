f0695affecc6bb946ac5a7875053225a17fe9ff4
HaibinWu666 <f07921092@ntu.edu.tw>
2022-06-13 04:31:53 +0000

    remove tmp file in the first-version trainer

--- s3prl/task/__init__.py
+++ s3prl/task/__init__.py
@@ -1,6 +1,2 @@
 from .base import Task
 from .dump_feature import DumpFeature
-
-from .speech2text_ctc import Speech2TextCTC
-from .utterance_classification import UtteranceClassification
-from .speaker_verification import SpeakerVerification
