f45e98cd77333a2f7e4ec1b15a2e98cfb3c55259
andi611 <tingweiandyliu@gmail.com>
2022-06-14 19:08:16 +0800

    upgrade identity nn

--- s3prl/nn/identity.py
+++ s3prl/nn/identity.py
@@ -1,9 +1,20 @@
 import torch.nn as nn
 
+from s3prl import Output
+
 
 class Identity(nn.Module):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(Identity, self).__init__()
 
-    def forward(self, output):
+    def forward(self, output: Output):
+        """
+        This nn module is used as a placeholder for certain interfaces (e.g. the predictor in ssl tasks).
+
+        Args:
+            output (Output): the s3prk Output object
+
+        Return:
+            output (Output): exactly the same as input, the s3prk Output object
+        """
         return output
