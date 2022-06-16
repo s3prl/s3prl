be93e977b8756178a715d49b190c9c2ab0ac458b
Leo Yang <leo19941227@gmail.com>
2022-06-16 16:37:08 +0800

    fix import order

--- s3prl/__init__.py
+++ s3prl/__init__.py
@@ -1,7 +1,8 @@
 from pathlib import Path as _Path
 
+from s3prl.base import *
+
 from . import nn
-from .base import *
 
 with (_Path(__file__).parent.resolve() / "version.txt").open() as file:
     __version__ = file.read().strip()
