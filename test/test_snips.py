fe72fde5ff46f517dd21ce59bbf0a8b54911bf68
Heng-Jui Chang <b06901020@ntu.edu.tw>
2022-06-25 14:43:03 +0800

    snips corpus

---
+++ test/test_snips.py
@@ -0,0 +1,15 @@
+import pytest
+from dotenv import dotenv_values
+
+from s3prl.corpus.snips import snips_for_speech2text
+
+
+@pytest.mark.corpus
+def test_snips():
+    config = dotenv_values()
+    dataset_root = config["SNIPS"]
+    dataset = snips_for_speech2text(dataset_root)
+
+    assert len(dataset.train_data) == 104672
+    assert len(dataset.valid_data) == 2800
+    assert len(dataset.test_data) == 2800
