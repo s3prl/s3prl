38163c1be85523c624c4f40470bcdfe1e9d70809
Leo Yang <leo19941227@gmail.com>
2022-06-28 04:51:44 +0800

    fix kaldi_datadir no corpus mark

--- test/test_kaldi_datadir.py
+++ test/test_kaldi_datadir.py
@@ -1,9 +1,11 @@
+import pytest
 from dotenv import dotenv_values
 
 from s3prl.corpus.kaldi import kaldi_for_multiclass_tagging
 from s3prl.dataset.multiclass_tagging import BuildMultiClassTagging
 
 
+@pytest.mark.corpus
 def test_kaldi_datadir_for_diar():
     kaldi_dir = dotenv_values()["Diarization"]
     train_data, valid_data, test_data = kaldi_for_multiclass_tagging(kaldi_dir).slice(3)
