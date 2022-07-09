b41b776c1d6cc5a75f1b5429a67a28166cfa8896
Leo Yang <leo19941227@gmail.com>
2022-07-09 13:51:45 +0800

    refine test for balanced weighted sampler

--- test/test_balanced_weighted_sampler.py
+++ test/test_balanced_weighted_sampler.py
@@ -1,32 +1,35 @@
+import pytest
 from collections import Counter
 
 from s3prl.sampler import BalancedWeightedSampler
 from s3prl.dataset.base import AugmentedDynamicItemDataset
 
+
 def test_balanced_weighted_sampler():
-    dataset = AugmentedDynamicItemDataset({
-        0: dict(
-            label="a"
-        ),
-        1: dict(
-            label="a"
-        ),
-        2: dict(
-            label="b"
-        ),
-        3: dict(
-            label="a"
-        ),
-    })
+    dataset = AugmentedDynamicItemDataset(
+        {
+            0: dict(label="a"),
+            1: dict(label="a"),
+            2: dict(label="b"),
+            3: dict(label="a"),
+        }
+    )
     batch_size = 5
-    sampler = BalancedWeightedSampler(dataset, batch_size=batch_size, duplicate=1000, seed=0)
-    indices = list(sampler)
-    assert len(indices[0]) == batch_size
+    prev_diff_ratio = 1.0
+    for duplicate in [10000, 100000]:
+        sampler = BalancedWeightedSampler(
+            dataset, batch_size=batch_size, duplicate=duplicate, seed=0
+        )
+        indices = list(sampler)
+        assert len(indices[0]) == batch_size
 
-    counter = Counter()
-    for batch_indices in indices:
-        for idx in batch_indices:
-            counter.update(dataset[idx]["label"])
+        counter = Counter()
+        for batch_indices in indices:
+            for idx in batch_indices:
+                counter.update(dataset[idx]["label"])
 
-    diff = abs(counter["a"] - counter["b"])
-    assert diff < 50
+        diff_ratio = abs(counter["a"] - counter["b"]) / duplicate * len(dataset)
+        assert diff_ratio < prev_diff_ratio
+        prev_diff_ratio = diff_ratio
+
+    diff_ratio < 0.05
