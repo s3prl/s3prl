8e415cf68d2a1b3ca7311efd681c628c9e56f43c
Leo Yang <leo19941227@gmail.com>
2022-08-12 23:09:31 +0800

    refine versioning

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
+with (here / "s3prl" / "version.txt").open() as file:
+    version = file.read()
 
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
@@ -71,7 +73,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.3.4",  # Required
+    version=version,  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
