3737d03e36569a4f57f8edecf07b3f248452f2dc
andi611 <tingweiandyliu@gmail.com>
2022-09-07 16:27:51 +0800

    add docstring for superb KS

--- s3prl/problem/common/superb_ks.py
+++ s3prl/problem/common/superb_ks.py
@@ -24,6 +24,90 @@ EFFECTS = [["channels", "1"], ["rate", "16000"], ["gain", "-3.0"]]
 class SuperbKS(Common):
     @classmethod
     def default_config(cls) -> dict:
+        """
+        Args:
+            start (int):
+                The starting stage of the problem script.
+                Default: 0
+            stop (int):
+                The stoping stage of the problem script, set `None` to reach the final stage.
+                Default: None
+            target_dir (str):
+                The directory that stores the script result.
+                Default: MISSING
+            cache_dir (str):
+                The directory that caches the processed data.
+                Default: /home/user/.cache/s3prl/data
+            remove_all_cache (bool):
+                Whether to remove all the cache stored under `cache_dir`.
+                Default: False
+            prepare_data (dict):
+                The dict that stores the prepare data config.
+                Default: dict(gsc1=MISSING,gsc1_test=MISSING,)
+            build_encoder (dict):
+                The dict that stores the encoder config.
+                Default: dict()
+            build_dataset (dict):
+                The dict that stores the dataset config.
+                Default: dict()
+            build_batch_sampler (dict):
+                The dict that stores the batch sampler config.
+                Default: dict(
+                    train=dict(batch_size=32,),
+                    valid=dict(batch_size=32,),
+                    test=dict(batch_size=32,),
+                )
+            build_upstream (dict):
+                The dict that stores the upstream config.
+                Default: dict(name="fbank",)
+            build_featurizer (dict):
+                The dict that stores the featurizer config.
+                Default: dict(layer_selections=None, normalize=False,)
+            build_downstream (dict):
+                The dict that stores the downstream config.
+                Default: dict(hidden_size=256,)
+            build_model (dict):
+                The dict that stores the model config.
+                Default: dict(upstream_trainable=False,)
+            build_task (dict):
+                The dict that stores the task config.
+                Default: dict()
+            build_optimizer (dict):
+                The dict that stores the optimizer config.
+                Default: dict(
+                    name="Adam",
+                    conf=dict(lr=1.0e-4,),
+                )
+            build_scheduler (dict):
+                The dict that stores the scheduler config.
+                Default: dict(name="ExponentialLR",gamma=0.9,)
+            save_model (dict):
+                The dict that stores the save model config.
+                Default: dict()
+            save_task (dict):
+                The dict that stores the save task config.
+                Default: dict()
+            train (dict):
+                The dict that stores the training script config.
+                Default: dict(
+                    total_steps=200000,
+                    log_step=100,
+                    eval_step=2000,
+                    save_step=500,
+                    gradient_clipping=1.0,
+                    gradient_accumulate_steps=1,
+                    valid_metric="accuracy",
+                    valid_higher_better=True,
+                    auto_resume=True,
+                    resume_ckpt_dir=None,
+                )
+            evaluate (dict):
+                The dict that stores the evaluation script config.
+                Default: dict()
+
+        Return:
+            default_config (dict): The dict that stores all the config
+        """
         return dict(
             start=0,
             stop=None,
@@ -79,7 +163,7 @@ class SuperbKS(Common):
                 gradient_clipping=1.0,
                 gradient_accumulate_steps=1,
                 valid_metric="accuracy",
-                valid_higher_better=False,
+                valid_higher_better=True,
                 auto_resume=True,
                 resume_ckpt_dir=None,
             ),
@@ -95,6 +179,31 @@ class SuperbKS(Common):
         gsc1_test: str,
         _get_path_only=False,
     ):
+        """
+        Args:
+            _target_dir (str):
+                The directory that stores the script result.
+            _cache_dir (str):
+                The directory that caches the processed data.
+            gsc1 (str):
+                The path that points to the gsc1 file.
+            gsc1_test (str):
+                The path that points to the gsc1 test file.
+            _get_path_only (bool):
+                Whether to prepare only the path and not the actual data.
+                Default: False
+
+        Return:
+            train_path (str):
+                The path that points to the prepared train csv file.
+                Default: "`_target_dir`/train.csv"
+            valid_path (str):
+                The path that points to the prepared valid csv file.
+                Default: "`_target_dir`/valid.csv"
+            test_paths (list[str]):
+                The path that points to the prepared test csv files.
+                Default: ["`_target_dir`/test.csv",]
+        """
         target_dir = Path(_target_dir)
 
         train_path = target_dir / f"train.csv"
@@ -167,6 +276,26 @@ class SuperbKS(Common):
         _test_csv_paths,
         _get_path_only=False,
     ):
+        """
+        Args:
+            _target_dir (str):
+                The directory that stores the script result.
+            _cache_dir (str):
+                The directory that caches the processed data.
+            _train_csv_path (str):
+                The path that points to the prepared train csv file.
+            _valid_csv_path (str):
+                The path that points to the prepared valid csv file.
+            _test_csv_paths (str):
+                The path that points to the prepared test csv files.
+            _get_path_only (bool):
+                Whether to prepare only the path and not the actual data.
+                Default: False
+
+        Return:
+            encoder (CategoryEncoders):
+                The builded encoder object.
+        """
         from s3prl.encoder.category import CategoryEncoder
 
         encoder_path = Path(_target_dir) / "encoder.pkl"
@@ -194,6 +323,24 @@ class SuperbKS(Common):
         _data_csv: str,
         _encoder_path: str,
     ):
+        """
+        Args:
+            _target_dir (str):
+                The directory that stores the script result.
+            _cache_dir (str):
+                The directory that caches the processed data.
+            _mode (str):
+                The mode of the dataset.
+                Default choices: ("train", "valid", "test")
+            _data_csv (str):
+                The path that points to the prepared (train/valid/test) csv file.
+            _encoder_path (str):
+                The path that points to the stored encoder object file.
+
+        Return:
+            dataset (UtteranceClassificationPipe):
+                The builded dataset object.
+        """
         data_points = OrderedDict()
         csv = pd.read_csv(_data_csv)
         for _, row in csv.iterrows():
@@ -241,6 +388,33 @@ class SuperbKS(Common):
         valid: dict = {},
         test: dict = {},
     ):
+        """
+        Args:
+            _target_dir (str):
+                The directory that stores the script result.
+            _cache_dir (str):
+                The directory that caches the processed data.
+            _mode (str):
+                The mode of the dataset.
+                Default choices: ("train", "valid", "test")
+            _data_csv (str):
+                The path that points to the prepared (train/valid/test) csv file.
+            _dataset (Dataset):
+                The dataset object.
+            train (dict):
+                The args for the batch sampler during "train" `_mode`.
+                Default: dict()
+            valid (dict):
+                The args for the batch sampler during "valid" `_mode`.
+                Default: dict()
+            test (dict):
+                The args for the batch sampler during "test" `_mode`.
+                Default: dict()
+
+        Return:
+            sampler (BalancedWeightedSampler):
+                The builded batch sampler object depended on the given `_mode`.
+        """
         from collections import Counter
 
         csv = pd.read_csv(_data_csv)
@@ -273,6 +447,22 @@ class SuperbKS(Common):
         _downstream_downsample_rate: int,
         hidden_size: int,
     ) -> AbsFrameModel:
+        """
+        Args:
+            _downstream_input_size (int):
+                The input size of the downstream model.
+            _downstream_output_size (int):
+                The output size of the downstream model.
+            _downstream_downsample_rate (int):
+                The downstream downsample rate.
+            hidden_size (int):
+                The hidden state size of the downstream model.
+
+        Return:
+            model (AbsFrameModel):
+                The builded downstream model object.
+                Defualt: MeanPoolingLinear
+        """
         model = MeanPoolingLinear(
             _downstream_input_size, _downstream_output_size, hidden_size
         )
