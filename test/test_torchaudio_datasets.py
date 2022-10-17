623d98f0c39646b050b193e2268f6f6295a8394f
Leo Yang <leo19941227@gmail.com>
2022-10-17 23:04:57 +0800

    try torchaudio.datasets librispeech

---
+++ test/test_torchaudio_datasets.py
@@ -0,0 +1,52 @@
+import logging
+from pathlib import Path
+
+import pytest
+from dotenv import dotenv_values
+
+from s3prl.dataio.corpus import LibriSpeech
+
+logger = logging.getLogger(__name__)
+
+try:
+    import torchaudio.datasets as datasets
+except ModuleNotFoundError:
+    test_torchaudio_dataset = False
+else:
+    test_torchaudio_dataset = True
+
+
+@pytest.mark.corpus
+def test_librispeech_with_torchaudio():
+    if not test_torchaudio_dataset:
+        return
+
+    config = dotenv_values()
+    dataset_root = Path(config["LibriSpeech"])
+    corpus = LibriSpeech(dataset_root)
+    train_data, valid_data, test_data = corpus.data_split
+
+    def get_s3prl_paths_and_labels(data: dict):
+        for key, value in data.items():
+            yield (
+                Path(value["wav_path"]).relative_to(dataset_root).as_posix(),
+                value["transcription"],
+            )
+
+    train_set = datasets.LIBRISPEECH(dataset_root.parent, "train-clean-100")
+    valid_set = datasets.LIBRISPEECH(dataset_root.parent, "dev-clean")
+    test_set = datasets.LIBRISPEECH(dataset_root.parent, "test-clean")
+
+    def get_torchaudio_paths_and_labels(dataset):
+        for i in range(len(dataset)):
+            path, sr, trans, spk, chapter, utt = dataset.get_metadata(i)
+            yield (path, trans)
+
+    def assert_corpus_parsing(data_s3prl: dict, data_torchaudio):
+        data_s3prl = sorted(get_s3prl_paths_and_labels(data_s3prl))
+        data_torchaudio = sorted(get_torchaudio_paths_and_labels(data_torchaudio))
+        assert data_s3prl == data_torchaudio
+
+    assert_corpus_parsing(train_data, train_set)
+    assert_corpus_parsing(valid_data, valid_set)
+    assert_corpus_parsing(test_data, test_set)
