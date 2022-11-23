80764ce7e9c6811035e9386e6dede9e9f8c63b7a
Leo Yang <leo19941227@gmail.com>
2022-11-23 10:19:09 +0800

    support fairseq checkpoint for wav2vec2 / hubert

--- s3prl/upstream/wav2vec2/hubconf.py
+++ s3prl/upstream/wav2vec2/hubconf.py
@@ -9,34 +9,60 @@
 
 import logging
 import os
+import time
+from pathlib import Path
+
+from filelock import FileLock
 
 from s3prl.util.download import _urls_to_filepaths
 
 from .expert import LegacyUpstreamExpert as _LegacyUpstreamExpert
 from .expert import UpstreamExpert as _UpstreamExpert
+from .convert import load_and_convert_fairseq_ckpt
 
 logger = logging.getLogger(__name__)
 
+NEW_ENOUGH_SECS = 2.0
+
 
 def wav2vec2_custom(
-    ckpt: str, *args, legacy: bool = False, refresh: bool = False, **kwargs
+    ckpt: str,
+    legacy: bool = False,
+    fairseq: bool = False,
+    refresh: bool = False,
+    **kwargs,
 ):
+    assert not (legacy and fairseq), (
+        "The option 'legacy' will directly load a fairseq checkpoint, "
+        "while the option 'fairseq' will first convert the fairseq checkpoint to "
+        "be fairseq indenpendent and then load the checkpoint. "
+        "These two options cannot be used jointly."
+    )
+
     if ckpt.startswith("http"):
         ckpt = _urls_to_filepaths(ckpt, refresh=refresh)
 
+    if fairseq:
+        ckpt: Path = Path(ckpt)
+        converted_ckpt = ckpt.parent / f"{ckpt.stem}.converted.pt"
+        lock_file = Path(str(converted_ckpt) + ".lock")
+
+        logger.info(f"Converting a fairseq checkpoint: {ckpt}")
+        logger.info(f"To: {converted_ckpt}")
+
+        with FileLock(str(lock_file)):
+            if not converted_ckpt.is_file() or (
+                refresh and (time.time() - os.path.getmtime(ckpt)) > NEW_ENOUGH_SECS
+            ):
+                load_and_convert_fairseq_ckpt(ckpt, converted_ckpt)
+
+        ckpt = converted_ckpt
+
     assert os.path.isfile(ckpt)
     if legacy:
-        return _LegacyUpstreamExpert(ckpt, *args, **kwargs)
+        return _LegacyUpstreamExpert(ckpt, **kwargs)
     else:
-        return _UpstreamExpert(ckpt, *args, **kwargs)
-
-
-def wav2vec2_local(*args, **kwargs):
-    return wav2vec2_custom(*args, **kwargs)
-
-
-def wav2vec2_url(*args, **kwargs):
-    return wav2vec2_custom(*args, **kwargs)
+        return _UpstreamExpert(ckpt, **kwargs)
 
 
 def wav2vec2(refresh=False, *args, **kwargs):
