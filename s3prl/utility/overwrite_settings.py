213f7445c75fc655fe12d21788d6601fe5ab9c9d
Leo Yang <leo19941227@gmail.com>
2023-02-22 15:30:53 +0800

    refine overwrite setting

--- s3prl/utility/overwrite_settings.py
+++ s3prl/utility/overwrite_settings.py
@@ -1,15 +1,16 @@
-import sys
+from pathlib import Path
+
 import yaml
 import argparse
 import torch
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ckpt_pth', required=True)
-parser.add_argument('--settings_dir', required=True)
-parser.add_argument('--new_ckpt_pth', required=True)
+parser.add_argument('settings_dir')
+parser.add_argument('ckpt_path')
+parser.add_argument('new_ckpt_path')
 paras = parser.parse_args()
 
-ckpt = torch.load(paras.ckpt_pth, map_location='cpu')
+ckpt = torch.load(paras.ckpt_path, map_location='cpu')
 
 with open(f'{paras.settings_dir}/args.yaml', 'r') as handle:
     args = yaml.load(handle, Loader=yaml.FullLoader)
@@ -19,4 +20,5 @@ with open(f'{paras.settings_dir}/config.yaml', 'r') as handle:
     config = yaml.load(handle, Loader=yaml.FullLoader)
     ckpt['Config'] = config
 
-torch.save(ckpt, paras.new_ckpt_pth)
+Path(paras.new_ckpt_path).parent.mkdir(exist_ok=True, parents=True)
+torch.save(ckpt, paras.new_ckpt_path)
