3f6aea52e2b6d19550e7f240160a5d9bb1ed8a93
Leo Yang <leo19941227@gmail.com>
2023-06-11 19:55:40 +0800

    fix fast_vgs

--- s3prl/upstream/fast_vgs/hubconf.py
+++ s3prl/upstream/fast_vgs/hubconf.py
@@ -12,9 +12,9 @@ logger = logging.getLogger(__name__)
 def fast_vgs_plus(**kwargs):
     tar_file: Path = Path(urls_to_filepaths("https://huggingface.co/s3prl/fast_vgs_plus/resolve/main/fast-vgs-plus-coco.zip"))
     target_dir = Path(tar_file.parent) / tar_file.stem
-    if target_dir.is_dir():
-        shutil.rmtree(target_dir)
+    if not target_dir.is_dir():
 
-    os.system(f"unzip {tar_file} -d {target_dir}")
+        os.system(f"unzip {tar_file} -d {target_dir}")
     kwargs["ckpt"] = target_dir / "fast-vgs-plus-coco"
     return _UpstreamExpert(**kwargs)
+
