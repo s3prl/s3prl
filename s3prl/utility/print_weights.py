9197ced16f18a8156c7b9c66bf23bddc5d14661a
Leo Yang <leo19941227@gmail.com>
2023-06-12 10:21:59 +0800

    add dimension

--- s3prl/utility/print_weights.py
+++ s3prl/utility/print_weights.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 
 ckpt = torch.load(args.ckpt, map_location="cpu")
 weights = ckpt["Featurizer"]["weights"]
-norm_weights = torch.nn.functional.softmax(weights)
+norm_weights = torch.nn.functional.softmax(weights, dim=-1)
 for weight in norm_weights.tolist():
     print(weight)
 
