12be3af1fb2f61e17a18d41d86258c3681af8d1d
chenjoachim <lllion66666@gmail.com>
2023-08-07 15:36:00 +0800

    Complete feature of weighted sum

---
+++ s3prl/nn/two_model_sum.py
@@ -0,0 +1,28 @@
+import torch
+import math
+
+
+class two_model_sum(torch.nn.Module):
+    def __init__(self):
+        """
+        In the constructor we instantiate four parameters and assign them as
+        member parameters.
+        l:weight
+        """
+        super().__init__()
+        self.l = torch.nn.Parameter(torch.zeros(()))
+
+    def forward(self, wavlm_feature, hubert_feature):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        result = []
+        # print(torch.sigmoid(self.l))
+        # exit()
+        l = torch.sigmoid(self.l)
+        for i in range(len(wavlm_feature)):
+            tmp = l*wavlm_feature[i] + (1-l)*hubert_feature[i]
+            result.append(tmp)
+        return result
