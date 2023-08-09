e2fd46d1c79a9e57f27f48a3d797d7b8913730d3
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2023-08-09 12:18:34 +0800

    Add preprocess/generate_len_for_bucket_librilight.py

---
+++ s3prl/preprocess/generate_len_for_bucket_librilight.py
@@ -0,0 +1,96 @@
+###############
+# IMPORTATION #
+###############
+import os
+import argparse
+import torchaudio
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from joblib import Parallel, delayed
+
+
+#############################
+# PREPROCESS CONFIGURATIONS #
+#############################
+def get_preprocess_args():
+    
+    parser = argparse.ArgumentParser(description='preprocess arguments for any dataset.')
+
+    parser.add_argument('-i', '--input_data', default='../LibriSpeech/', type=str, help='Path to your LibriSpeech directory', required=False)
+    parser.add_argument('-o', '--output_path', default='./data/', type=str, help='Path to store output', required=False)
+    parser.add_argument('-a', '--audio_extension', default='.flac', type=str, help='audio file type (.wav / .flac / .mp3 / etc)', required=False)
+    parser.add_argument('-n', '--name', default='len_for_bucket', type=str, help='Name of the output directory', required=False)
+    parser.add_argument('--n_jobs', default=-1, type=int, help='Number of jobs used for feature extraction', required=False)
+
+    args = parser.parse_args()
+    return args
+
+
+##################
+# EXTRACT LENGTH #
+##################
+def extract_length(input_file):
+    torchaudio.set_audio_backend("sox_io")
+    return torchaudio.info(input_file).num_frames
+
+
+###################
+# GENERATE LENGTH #
+###################
+def generate_length(args, tr_set, audio_extension):
+    
+    for i, s in enumerate(tr_set):
+        if os.path.isdir(os.path.join(args.input_data, s.lower())):
+            s = s.lower()
+        elif os.path.isdir(os.path.join(args.input_data, s.upper())):
+            s = s.upper()
+        else:
+            assert NotImplementedError
+
+        print('')
+        todo = list(Path(os.path.join(args.input_data, s)).rglob('*' + audio_extension)) # '*.flac'
+        print(f'Preprocessing data in: {s}, {len(todo)} audio files found.')
+
+        output_dir = os.path.join(args.output_path, args.name)
+        if not os.path.exists(output_dir): os.makedirs(output_dir)
+
+        print('Extracting audio length...', flush=True)
+        tr_x = Parallel(n_jobs=args.n_jobs)(delayed(extract_length)(str(file)) for file in tqdm(todo))
+
+        # sort by len
+        sorted_todo = [os.path.join(s, str(todo[idx]).split(s+'/')[-1]) for idx in reversed(np.argsort(tr_x))]
+        # Dump data
+        df = pd.DataFrame(data={'file_path':[fp for fp in sorted_todo], 'length':list(reversed(sorted(tr_x))), 'label':None})
+        df.to_csv(os.path.join(output_dir, tr_set[i] + '.csv'))
+
+    print('All done, saved at', output_dir, 'exit.')
+
+
+########
+# MAIN #
+########
+def main():
+
+    # get arguments
+    args = get_preprocess_args()
+    
+    # change the SETS list to match your dataset, for example:
+    # SETS = ['train', 'dev', 'test']
+    # SETS = ['TRAIN', 'TEST']
+    # SETS = ['train-clean-100', 'train-clean-360', 'train-other-500', 'dev-clean', 'dev-other', 'test-clean', 'test-other']
+    SETS = ['clean', 'other']
+    
+    # Select data sets
+    for idx, s in enumerate(SETS):
+        print('\t', idx, ':', s)
+    tr_set = input('Please enter the index of splits you wish to use preprocess. (seperate with space): ')
+    tr_set = [SETS[int(t)] for t in tr_set.split(' ')]
+
+    # Acoustic Feature Extraction & Make Data Table
+    generate_length(args, tr_set, args.audio_extension)
+
+
+if __name__ == '__main__':
+    main()
