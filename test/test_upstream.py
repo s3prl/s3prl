d7e7b272524bbecfd89ef20498db6dc6b8015abf
Leo Yang <leo19941227@gmail.com>
2023-10-26 00:23:19 +0800

    prevent circular dependency

--- test/test_upstream.py
+++ test/test_upstream.py
@@ -209,7 +209,6 @@ def _test_specific_upstream(name: str):
         "decoar_layers",
         "decoar2",
         "distilhubert",
-        "espnet_hubert_base_iter1",
         "hubert",
         "lighthubert_base",
         "mockingjay",
