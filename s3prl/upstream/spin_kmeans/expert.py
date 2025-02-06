f32e2da9b04275aec1dce952dddcf19e33cb3910
Shu-wen Yang <leo19941227@gmail.com>
2025-02-06 10:26:17 +0800

    support spin kmeans

--- s3prl/upstream/spin_kmeans/expert.py
+++ s3prl/upstream/spin_kmeans/expert.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 
 from ..spin import hubconf
 
+EMBEDDING_SIZE = 256
+
 
 class ApplyKmeans(nn.Module):
     def __init__(self, km_path):
@@ -18,26 +20,60 @@ class ApplyKmeans(nn.Module):
 
     def __call__(self, x):
         dist = x.pow(2).sum(1, keepdim=True) - 2 * torch.matmul(x, self.C) + self.Cnorm
-        cluster_ids = dist.argmin(dim=1).cpu()
+        cluster_ids = dist.argmin(dim=1)
         codewords = self.C.transpose(0, 1)[cluster_ids, :]
-        return codewords
+        return codewords, cluster_ids
 
 
 class UpstreamExpert(nn.Module):
-    def __init__(self, spin_name, km_path):
+    def __init__(self, spin_name, km_path, stack_num=None, cluster_num: int = 1000):
         super().__init__()
         self.spin = getattr(hubconf, spin_name)()
         self.km = ApplyKmeans(km_path)
+        self.stack_num = stack_num
+        self.embeddings = None
+        if stack_num is not None:
+            self.embeddings = nn.Embedding(cluster_num, EMBEDDING_SIZE)
 
     def get_downsample_rates(self, key: str) -> int:
+        if self.stack_num is not None:
+            return round(self.spin.get_downsample_rates(key) * self.stack_num)
         return self.spin.get_downsample_rates(key)
+
+    def train(self, mode=True):
+        if mode:
+            self.eval()
+            self.requires_grad_(False)
+            if self.embeddings is not None:
+                self.embeddings.train()
+            return self
+        else:
+            return super().train(mode)
 
     def forward(self, wavs):
         hs = self.spin(wavs)["hidden_states"][-1]
         bs, seqlen, size = hs.shape
         hs = hs.reshape(bs * seqlen, size)
-        codewords = self.km(hs)
+        codewords, cluster_ids = self.km(hs)
         codewords = codewords.reshape(bs, seqlen, size)
-        return {
-            "hidden_states": [codewords],
-        }
+        cluster_ids = cluster_ids.reshape(bs, seqlen)
+
+        if self.embeddings is None:
+            return {
+                "hidden_states": [codewords],
+            }
+        else:
+            bsz, seqlen = cluster_ids.shape
+            cluster_ids = (
+                cluster_ids[:, : seqlen // self.stack_num * self.stack_num]
+                .reshape(bsz, seqlen // self.stack_num, self.stack_num)
+                .permute(2, 0, 1)
+            )
+            embs = []
+            for idx, cluster_id in enumerate(cluster_ids):
+                emb = self.embeddings(cluster_id)
+                embs.append(emb)
+            embs = torch.cat(embs, dim=-1)
+            return {
+                "hidden_states": [embs],
+            }
--- s3prl/upstream/spin_kmeans/hubconf.py
+++ s3prl/upstream/spin_kmeans/hubconf.py
@@ -15,3 +15,30 @@ def spin_hubert_2048_kmeans_1000(refresh=False, **kwargs):
         "/home/leo1994122701/cslm/cst/result/km_models/spin_hubert_2048_km1000"  # TODO: to url
     )
     return UpstreamExpert(**kwargs)
+
+
+def spin_hubert_2048_kmeans_1000_stack2(refresh=False, **kwargs):
+    kwargs["spin_name"] = "spin_hubert_2048"
+    kwargs["km_path"] = (
+        "/home/leo1994122701/cslm/cst/result/km_models/spin_hubert_2048_km1000"  # TODO: to url
+    )
+    kwargs["stack_num"] = 2
+    return UpstreamExpert(**kwargs)
+
+
+def spin_hubert_2048_kmeans_1000_stack4(refresh=False, **kwargs):
+    kwargs["spin_name"] = "spin_hubert_2048"
+    kwargs["km_path"] = (
+        "/home/leo1994122701/cslm/cst/result/km_models/spin_hubert_2048_km1000"  # TODO: to url
+    )
+    kwargs["stack_num"] = 4
+    return UpstreamExpert(**kwargs)
+
+
+def spin_hubert_2048_kmeans_1000_stack8(refresh=False, **kwargs):
+    kwargs["spin_name"] = "spin_hubert_2048"
+    kwargs["km_path"] = (
+        "/home/leo1994122701/cslm/cst/result/km_models/spin_hubert_2048_km1000"  # TODO: to url
+    )
+    kwargs["stack_num"] = 8
+    return UpstreamExpert(**kwargs)
