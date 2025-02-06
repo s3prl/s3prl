9e3af1cce0db10ac5844d122b4536cb7dc973b8d
Shu-wen Yang <leo19941227@gmail.com>
2025-02-06 23:36:36 +0800

    support lower dimentional latents

--- s3prl/upstream/vae/hubconf.py
+++ s3prl/upstream/vae/hubconf.py
@@ -7,8 +7,30 @@ def ae_lr5e_4_mlp_latent(*args, **kwargs):
         "latent",
     )
 
+
 def ae_lr5e_4_mlp_reconstruct(*args, **kwargs):
     return UpstreamExpert(
         "/home/leo1994122701/cslm/cst/result/vae/ae_lr5e-4_mlp/best_valid_loss--0.675.ckpt",
         "reconstruct",
+    )
+
+
+def ae_lr5e_4_1transformer_latent64(*args, **kwargs):
+    return UpstreamExpert(
+        "/home/leo1994122701/cslm/cst/result/vae/ae_lr5e-4_1transformer_latent64/best_valid_loss--1.176.ckpt",
+        "latent",
+    )
+
+
+def ae_lr5e_4_1transformer_latent32(*args, **kwargs):
+    return UpstreamExpert(
+        "/home/leo1994122701/cslm/cst/result/vae/ae_lr5e-4_1transformer_latent32/best_valid_loss--1.049.ckpt",
+        "latent",
+    )
+
+
+def ae_lr5e_4_1transformer_latent16(*args, **kwargs):
+    return UpstreamExpert(
+        "/home/leo1994122701/cslm/cst/result/vae/ae_lr5e-4_1transformer_latent16/best_valid_loss--0.922.ckpt",
+        "latent",
     )
